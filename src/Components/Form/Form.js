baae97f9ac3af7fb3584531a93fdf25804ee4187
Ivonne Hernandez <ivonne702@gmail.com>
2022-04-19 08:06:16 -0600

    Add a Form component

---
+++ src/Components/Form/Form.js
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import Other from '../Other/Other';
+import PersonalInfo from '../PersonalInfo/PersonalInfo';
+import SignUpInfo from '../SignUpInfo/SignUpInfo';
+import './Form.css';
+
+const Form = () => {
+  const [page, setPage] = useState(0);
+
+  const FormTitles = ['Sign Up', 'Personal Info', 'Other'];
+
+  const pageDisplay = () => {
+    if (page === 0) {
+      return <SignUpInfo />
+    } else if (page === 1) {
+      return <PersonalInfo />
+    } else {
+      return <Other />
+    }
+  }
+
+  return (
+    <div className='benefits-form'>
+      <div className='progress-bar'></div>
+      <div className='form-container'>
+        <div className='header'>
+          <h2>{FormTitles[page]}</h2>
+        </div>
+        <div className='body'>
+          {pageDisplay()}
+        </div>
+        <div className='footer'></div>
+          <button
+            disabled={page === 0}
+            onClick={() => {
+              setPage((currentPage) => currentPage - 1);
+            }}
+          >
+            Prev
+          </button>
+          <button 
+            disabled={page === FormTitles.length - 1}
+            onClick={() => {
+              setPage((currentPage) => currentPage + 1);
+            }}
+          >
+            Next
+          </button>
+      </div>
+    </div>
+  );
+}
+
+export default Form;
