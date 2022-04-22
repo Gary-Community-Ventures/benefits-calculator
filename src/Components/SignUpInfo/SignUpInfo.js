f0744f792491c3cb07d1a91ba216d2104e994e74
Ivonne Hernandez <ivonne702@gmail.com>
2022-04-22 11:12:39 -0600

    Add error and helperText to StyledTextFields, remove id prop

--- src/Components/SignUpInfo/SignUpInfo.js
+++ src/Components/SignUpInfo/SignUpInfo.js
@@ -11,34 +11,37 @@ const SignUpInfo = ({ formData, setFormData }) => {
     <div className='sign-up-container'>
       <StyledTextField 
         type='email'
-        id='email'
         name='email'
         value={formData.email}
         label='Email'
-        onChange={(event) => {setFormData({ ...formData, email: event.target.value })}}
+        onChange={(event) => {setFormData({ ...formData, email: event.target.value})}}
         variant='outlined'
         size='small'
-        required />
+        required 
+        error={!/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email)}
+        helperText={!/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email) ? 'Invalid email.' : ''} />
       <StyledTextField 
         type='password'
-        id='password'
         name='password'
         value={formData.password}
         label='Password'
-        onChange={(event) => {setFormData({ ...formData, password: event.target.value })}}
+        onChange={(event) => {setFormData({ ...formData, password: event.target.value})}}
         variant='outlined'
         size='small'
-        required />
+        required 
+        error={formData.password === ''}
+        helperText={formData.password === '' ? 'Please enter a password' : ''} />
       <StyledTextField 
         type='password'
-        id='confirmed-password'
         name='confirmPassword'
         value={formData.confirmPassword}
         label='Confirm Password'
-        onChange={(event) => {setFormData({ ...formData, confirmPassword: event.target.value })}}
+        onChange={(event) => {setFormData({ ...formData, confirmPassword: event.target.value})}}
         variant='outlined'
         size='small'
-        required />
+        required 
+        error={formData.confirmPassword === '' || formData.confirmPassword !== formData.password} 
+        helperText={formData.confirmPassword === '' || formData.confirmPassword !== formData.password ? 'Please enter a matching password.' : ''} />
     </div>
   );
 }
