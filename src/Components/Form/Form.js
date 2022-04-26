a8caf7d8cd3517e7c66bcdd914303328ff9ff6b0
Ivonne Hernandez <ivonne702@gmail.com>
2022-04-26 15:47:27 -0600

    Remove form validation functions from Form.js and add to each component's onClick

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -25,39 +25,6 @@ const Form = () => {
     setFormData({ ...formData, [name]:  value });
   };
   
-  const handleSubmit = (event) => {
-    console.log('in handleSubmit')
-    event.preventDefault();
-    if (validateInputs()) {
-      console.log(formData, `We should submit this to our BE here via a POST request`)
-    }
-  }
-
-  const validateInputs = () => {
-    console.log(`in validateInputs`, validateEmail() && validatePassword() && validateConfirmPassword() && validateFirstLastUserNames() && validateNumChildrenAdults())
-    return validateEmail() && validatePassword() && validateConfirmPassword() && validateFirstLastUserNames() && validateNumChildrenAdults();
-  }
-
-  const validateEmail = () => {
-    return !/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email);
-  }
-
-  const validatePassword = () => {
-    return formData.password.length > 0;
-  }
-
-  const validateConfirmPassword = () => {
-    return formData.confirmPassword.length > 0 && formData.confirmPassword === formData.password;
-  }
-
-  const validateFirstLastUserNames = () => {
-    return formData.firstName.length > 0 && formData.lastName.length > 0 && formData.username.length > 0;
-  }
-
-  const validateNumChildrenAdults = () => {
-    return Number(formData.numberOfChildren) >= 0 && Number(formData.numberOfAdults) >= 0;
-  }
-
   const displayPage = (pageIndex) => {
     switch(pageIndex) {
       case 0:
