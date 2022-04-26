ed3ba5d2bf091ed9c4e3abd9faa93387ed30c23a
Ivonne Hernandez <ivonne702@gmail.com>
2022-04-26 15:57:35 -0600

    Refactor Success message

--- src/Components/Success/Success.js
+++ src/Components/Success/Success.js
@@ -2,7 +2,7 @@ const Success = () => {
   return (
     <div className='success-container'>
       <h1>Thank you for your submission.</h1>
-      <p>You will get a confirmation email with further instructions.</p>
+      <p>You will get an email confirmation with further instructions.</p>
     </div>
   );
 }
