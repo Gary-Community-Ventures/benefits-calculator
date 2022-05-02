20388ce4ba796d9ec8f7dbf8795b6d6b9d2df577
Ivonne Hernandez <ivonne702@gmail.com>
2022-05-02 10:08:25 -0600

    Add conditional rendering to error, helperText in SignUpInfo.js using isSubmitting variable

--- src/Components/SignUpInfo/SignUpInfo.js
+++ src/Components/SignUpInfo/SignUpInfo.js
@@ -41,8 +41,8 @@ const SignUpInfo = ({ formData, handleChange, page, setPage }) => {
         variant='outlined'
         size='small'
         required 
-        error={!/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email)}
-        helperText={!/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email) ? 'Invalid email.' : ''} />
+        error={formData.isSubmitting && !/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email)}
+        helperText={formData.isSubmitting && !/^.+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/.test(formData.email) ? 'Invalid email.' : ''} />
       <StyledTextField 
         type='password'
         name='password'
@@ -52,8 +52,8 @@ const SignUpInfo = ({ formData, handleChange, page, setPage }) => {
         variant='outlined'
         size='small'
         required 
-        error={formData.password === ''}
-        helperText={formData.password === '' ? 'Please enter a password.' : ''} />
+        error={formData.isSubmitting && formData.password === ''}
+        helperText={formData.isSubmitting && formData.password === '' ? 'Please enter a password.' : ''} />
       <StyledTextField 
         type='password'
         name='confirmPassword'
@@ -63,8 +63,8 @@ const SignUpInfo = ({ formData, handleChange, page, setPage }) => {
         variant='outlined'
         size='small'
         required 
-        error={formData.confirmPassword === '' || formData.confirmPassword !== formData.password} 
-        helperText={formData.confirmPassword === '' || formData.confirmPassword !== formData.password ? 'Please enter a matching password.' : ''} />
+        error={(formData.isSubmitting && formData.confirmPassword === '') || (formData.confirmPassword !== formData.password)} 
+        helperText={(formData.isSubmitting && formData.confirmPassword === '') || (formData.confirmPassword !== formData.password ? 'Please enter a matching password.' : '')} />
         <Button
           onClick={(event) => {handleSubmit(event)}}
           variant='contained'>
