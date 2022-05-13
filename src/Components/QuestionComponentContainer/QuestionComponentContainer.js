ba4f668c9354156172d775d7245058c4b618c562
Ivonne Hernandez <ivonne702@gmail.com>
2022-05-13 11:08:44 -0600

    Add Previous and Continue buttons to QuestionComponentContainer

--- src/Components/QuestionComponentContainer/QuestionComponentContainer.js
+++ src/Components/QuestionComponentContainer/QuestionComponentContainer.js
@@ -1,29 +1,42 @@
 import Textfield from '../Textfield/Textfield';
+import PreviousButton from '../PreviousButton/PreviousButton';
+import ContinueButton from '../ContinueButton/ContinueButton';
 import questions from '../../Assets/questions';
 import './QuestionComponentContainer.css';
 
 const QuestionComponentContainer = ({ formData, handleChange, handleSubmit, page, setPage }) => {
-  const createQuestionComponent = () => {
-    const matchingQuestion = questions.find((question) => question.id === page);
-
-    if (matchingQuestion.componentDetails.componentType === 'Textfield') {
-      return (
-        <>
-          <p className='question-label'>{matchingQuestion.question}</p>
-          <Textfield 
-            componentDetails={matchingQuestion.componentDetails}
-            formData={formData}
-            handleChange={handleChange} />
-        </>
-      );
-    }
+  const matchingQuestion = questions.find((question) => question.id === page);
+  console.log({matchingQuestion})
+  if (matchingQuestion.componentDetails.componentType === 'Textfield') {
+    return (
+      <div className='question-container' id={matchingQuestion.id}>
+        <p className='question-label'>{matchingQuestion.question}</p>
+        <Textfield 
+          componentDetails={matchingQuestion.componentDetails}
+          formData={formData}
+          handleChange={handleChange} />
+        {matchingQuestion.id === 0 ? 
+          <ContinueButton 
+            handleSubmit={handleSubmit} 
+            inputError={matchingQuestion.componentDetails.inputError}
+            formData={formData} 
+            inputName={matchingQuestion.componentDetails.inputName} />
+        
+        : 
+          <div className='question-buttons'>
+            <PreviousButton 
+              page={page} 
+              setPage={setPage} />
+            <ContinueButton 
+              handleSubmit={handleSubmit} 
+              inputError={matchingQuestion.componentDetails.inputError}
+              formData={formData} 
+              inputName={matchingQuestion.componentDetails.inputName} />
+          </div>
+        }
+      </div>
+    );
   }
-
-  return (
-    <>
-      {createQuestionComponent(page)}
-    </>
-  )
 
 }
 
