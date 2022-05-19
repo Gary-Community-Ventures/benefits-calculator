b1fa2594de5ed4a3173c64ebd5018b3990a2f968
Ivonne Hernandez <ivonne702@gmail.com>
2022-05-19 14:17:13 -0600

    Add Radiofield case to QuestionComponentContainer

--- src/Components/QuestionComponentContainer/QuestionComponentContainer.js
+++ src/Components/QuestionComponentContainer/QuestionComponentContainer.js
@@ -1,10 +1,11 @@
 import Textfield from '../Textfield/Textfield';
+import Radiofield from '../Radiofield/Radiofield';
 import PreviousButton from '../PreviousButton/PreviousButton';
 import ContinueButton from '../ContinueButton/ContinueButton';
 import questions from '../../Assets/questions';
 import './QuestionComponentContainer.css';
 
-const QuestionComponentContainer = ({ formData, handleChange, handleSubmit, page, setPage }) => {
+const QuestionComponentContainer = ({ formData, handleChange, handleSubmit, page, setPage, handleRadioButtonChange }) => {
   const matchingQuestion = questions.find((question) => question.id === page);
   console.log({matchingQuestion})
   if (matchingQuestion.componentDetails.componentType === 'Textfield') {
@@ -36,6 +37,26 @@ const QuestionComponentContainer = ({ formData, handleChange, handleSubmit, page
         }
       </div>
     );
+  } else if (matchingQuestion.componentDetails.componentType === 'Radiofield') {
+    return (
+      <div className='question-container' id={matchingQuestion.id}>
+        <p className='question-label'>{matchingQuestion.question}</p>
+        <Radiofield 
+          componentDetails={matchingQuestion.componentDetails}
+          formData={formData}
+          handleRadioButtonChange={handleRadioButtonChange} />
+        <div className='question-buttons'>
+          <PreviousButton 
+            page={page} 
+            setPage={setPage} />
+          <ContinueButton 
+            handleSubmit={handleSubmit} 
+            inputError={matchingQuestion.componentDetails.inputError}
+            formData={formData} 
+            inputName={matchingQuestion.componentDetails.inputName} />
+        </div>
+      </div>
+    ); 
   }
 
 }
