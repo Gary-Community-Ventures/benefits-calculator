e6863334af5adffb68ed7b3d5c8ab005333d6d56
Ivonne Hernandez <ivonne702@gmail.com>
2022-05-19 14:20:46 -0600

    Refactor handleSubmit, displayPage, remove isStudent from state

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -1,8 +1,5 @@
 import { useState } from 'react';
-import QuestionOne from '../QuestionOne/QuestionOne';
-import QuestionTwo from '../QuestionTwo/QuestionTwo';
-import QuestionThree from '../QuestionThree/QuestionThree';
-import QuestionFour from '../QuestionFour/QuestionFour';
+import QuestionComponentContainer from '../QuestionComponentContainer/QuestionComponentContainer';
 import './Form.css';
 
 const Form = () => {
@@ -11,7 +8,6 @@ const Form = () => {
   const [formData, setFormData] = useState({
     applicantAge: 0,
     zipcode: '',
-    isStudent: false,
     isAFullTimeStudent: false,
     isPregnant: false,
     isUnemployed: false,
@@ -40,42 +36,29 @@ const Form = () => {
     let boolValue = (value === 'true');
     setFormData({ ...formData, [name]: boolValue });
   }
-  
-  const displayPage = (pageIndex) => {
-    switch(pageIndex) {
-      case 0:
-        return <QuestionOne 
-                formData={formData}
-                handleChange={handleChange} 
-                page={page}
-                setPage={setPage} />
-      case 1:
-        return <QuestionTwo
-                formData={formData}
-                handleChange={handleChange} 
-                page={page}
-                setPage={setPage} />
-      case 2:
-        return <QuestionThree 
-                formData={formData}
-                handleCheckboxChange={handleCheckboxChange} 
-                page={page}
-                setPage={setPage}
-                handleRadioButtonChange={handleRadioButtonChange} />
-      case 3:
-        return <QuestionFour
-                formData={formData}
-                handleCheckboxChange={handleCheckboxChange} 
-                page={page}
-                setPage={setPage} />
+
+  const handleSubmit = (event, validateInputFunction, inputToBeValidated) => {
+    event.preventDefault();
+    if (!validateInputFunction(inputToBeValidated)) {
+      setPage(page + 1);
     }
-  }
+  }  
+  
+  const displayPage = () => {
+    return <QuestionComponentContainer 
+            formData={formData} 
+            handleChange={handleChange} 
+            handleSubmit={handleSubmit}
+            page={page}
+            setPage={setPage} 
+            handleRadioButtonChange={handleRadioButtonChange} /> 
+  }  
 
   return (
     <main className='benefits-form'>
       <p className='step-progress-title'>Step {page + 1} of 7</p>
       <h2 className='sub-header'>Tell us a little more about yourself.</h2>
-        {displayPage(page)}
+        {displayPage()}
     </main>
   );
 }
