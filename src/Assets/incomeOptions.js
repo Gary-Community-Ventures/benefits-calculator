e55c3e212da4d7c4e45c85a988dd2c6862d00b9f
Ivonne Hernandez <ivonne702@gmail.com>
2022-05-25 16:14:49 -0600

    Add incomeOptions.js file

---
+++ src/Assets/incomeOptions.js
@@ -0,0 +1,24 @@
+const incomeOptions = {
+  wages: 'wages, salaries, tips',
+  selfEmployment: 'self-employment Income',
+  unemployment: 'unemployment benefits',
+  cashAssistance: 'Cash Assistance grant',
+  childSupport: 'child support (received)',
+  disabilityMedicaid: 'disability-related Medicaid',
+  sSI: 'Supplemental Security Income (SSI)',
+  sSDependent: 'Social Security Dependent Benefits',
+  sSDisability: 'Social Security Disability Benefits',
+  sSSurvivor: "Social Security Survivor's Benefits",
+  sSRetirement: 'Social Security Retirement Benefits',
+  cOSDisability: 'Colorado State Disability Benefits',
+  veteran: "Veteran's Pension or Benefits",
+  pension: 'Government or Private Pension',
+  deferredComp: 'Withdrawals from Deferred Compensation (IRA, Keogh, etc.)',
+  workersComp: "Worker's Compensation",
+  alimony: 'alimony (received)',
+  boarder: 'boarder or lodger',
+  gifts: 'gifts/contributions (received)',
+  rental: 'rental income',
+  investment: 'investment income (interest, dividends, and profit from selling stocks)'
+}
+export default incomeOptions;
