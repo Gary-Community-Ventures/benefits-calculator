1ea90633e0633f04d04e2da2e6a58f64cf2ddd58
Ivonne Hernandez <ivonne702@gmail.com>
2022-05-31 10:11:55 -0600

    Add radiogroup-container class and rule to Radiofield

--- src/Components/Radiofield/Radiofield.js
+++ src/Components/Radiofield/Radiofield.js
@@ -1,5 +1,6 @@
 import {FormControlLabel, RadioGroup, Radio } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import './Radiofield.css';
 
 const StyledFormControlLabel = styled(FormControlLabel)({
   marginLeft: -5
@@ -8,14 +9,16 @@ const StyledFormControlLabel = styled(FormControlLabel)({
 const Radiofield = ({ componentDetails, formData, handleRadioButtonChange }) => {
   const { ariaLabel, inputName } = componentDetails;
   return (
-    <RadioGroup
-      aria-labelledby={ariaLabel}
-      name={inputName}
-      value={formData[inputName]}
-      onChange={handleRadioButtonChange} >
-        <StyledFormControlLabel value='true' control={<Radio />} label='Yes' />
-        <StyledFormControlLabel value='false' control={<Radio />} label='No' />
-    </RadioGroup>
+    <div className='radiogroup-container'>
+      <RadioGroup
+        aria-labelledby={ariaLabel}
+        name={inputName}
+        value={formData[inputName]}
+        onChange={handleRadioButtonChange} >
+          <StyledFormControlLabel value='true' control={<Radio />} label='Yes' />
+          <StyledFormControlLabel value='false' control={<Radio />} label='No' />
+      </RadioGroup>
+    </div>
   );
 }
 
