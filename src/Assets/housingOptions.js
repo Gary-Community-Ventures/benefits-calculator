73ba339bd6a3a39b929fcd3a3b68e4e57e642ed4
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-02 14:43:41 -0600

    Add housingOptions.js file

---
+++ src/Assets/housingOptions.js
@@ -0,0 +1,34 @@
+const housingOptions =  [
+  {
+    housingName: 'renting', 
+    housingLabel: 'Renting', 
+    isChecked: false
+  },
+  {
+    housingName: 'owner', 
+    housingLabel: 'You or a household member owns the home or apartment', 
+    isChecked: false
+  },
+  {
+    housingName: 'stayingWithFriend', 
+    housingLabel: 'Staying with friend', 
+    isChecked: false
+  },
+  {
+    housingName: 'hotel', 
+    housingLabel: 'In a hotel', 
+    isChecked: false
+  },
+  {
+    housingName: 'shelter', 
+    housingLabel: 'In a shelter or homeless', 
+    isChecked: false
+  },
+  {
+    housingName: 'preferNotToSay', 
+    housingLabel: 'Prefer not to say', 
+    isChecked: false
+  }
+];
+
+export default housingOptions;
