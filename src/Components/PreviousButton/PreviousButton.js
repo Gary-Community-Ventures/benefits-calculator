302304755312a3f7be1d6e639255d1822e8b5312
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-14 15:26:05 -0600

    Refactor PreviousButton to useParams and navigate based on id, remove page, setPage

--- src/Components/PreviousButton/PreviousButton.js
+++ src/Components/PreviousButton/PreviousButton.js
@@ -1,10 +1,19 @@
 import { Button } from '@mui/material';
+import { useNavigate, useParams } from 'react-router-dom';
 
-const PreviousButton = ({ page, setPage }) => {
+const PreviousButton = () => {
+  let { id } = useParams();
+  let numberId = Number(id);
+  let navigate = useNavigate();
+
   return (
     <Button
       onClick={() => {
-        setPage(page - 1);
+        if (numberId === 2) { //if it's the first question, navigate to the homepage
+          navigate('/')
+        } else {
+          navigate(`/question-${numberId - 1}`);
+        }
       }}
       variant='contained'>
       Prev
