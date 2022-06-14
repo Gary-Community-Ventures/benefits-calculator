94b76525591ef3629dd02edfd113b47c6f52938f
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-14 11:29:36 -0600

    Move state and functions from Form to App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,95 @@ import { Routes, Route } from 'react-router-dom';
 import Results from './Components/Results/Results';
 
 const App = () => {
+  const [page, setPage] = useState(0);
+
+  const [formData, setFormData] = useState({
+    agreeToTermsOfService: false,
+    applicantAge: 0,
+    zipcode: '',
+    student: false,
+    studentFulltime: false,
+    isPregnant: false,
+    unemployed: false,
+    unemployedWorkedInLast18Mos: false,
+    isBlindOrVisuallyImpaired: false,
+    isDisabled: false,
+    isAVeteran: false,
+    isOnMedicaid: false,
+    isOnDisabilityRelatedMedicaid: false,
+    hasIncome: false,
+    incomeStreams: [],
+    hasExpenses: false,
+    expenses: [],
+    householdSize: 0,
+    householdAssets: 0,
+    housing: {}
+  });
+
+  useEffect(() => {
+
+    if (formData.student === false) {
+      setFormData({ ...formData, studentFulltime: false });
+    }
+
+    if (formData.unemployed === false) { 
+      setFormData({ ...formData, unemployedWorkedInLast18Mos: false });
+    }
+
+    if(formData.hasIncome === false) {
+      setFormData({ ...formData, incomeStreams: [] });
+    }
+
+    if(formData.hasExpenses === false) {
+      setFormData({ ...formData, expenses: [] });
+    }
+    
+  }, [formData.student, formData.unemployed, formData.hasIncome, formData.hasExpenses]);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name !== 'zipcode') {
+      setFormData({ ...formData, [name]: Number(value) });
+    } else {
+      setFormData({ ...formData, [name]: value });
+    }
+  }
+
+  const handleCheckboxChange = (event) => {
+    //the value is the name of the formData property
+    const { value } = event.target; 
+    setFormData({ ...formData, [value]: !formData[value] });
+  }
+  
+  const handleRadioButtonChange = (event) => {
+    const { name, value } = event.target;
+    let boolValue = (value === 'true');
+    setFormData({ ...formData, [name]: boolValue });
+  }
+  
+  const handleSubmit = (event, validateInputFunction, inputToBeValidated) => {
+    event.preventDefault();
+    if (!validateInputFunction(inputToBeValidated)) {
+      setPage(page + 1);
+    }
+  }  
+
+  const handleHousingSourcesSubmit = (validatedHousingSources) => {
+    setFormData({ ...formData, housing: validatedHousingSources });
+    setPage(page + 1);
+  }
+  
+  const handleIncomeStreamsSubmit = (validatedIncomeStreams) => {
+    setFormData({ ...formData, incomeStreams: validatedIncomeStreams });
+    setPage(page + 1);
+  }
+
+  const handleExpenseSourcesSubmit = (validatedExpenseSources) => {
+    setFormData({ ...formData, expenses: validatedExpenseSources });
+    setPage(page + 1);
+  }
+
   return (
     <div className='App'>
         <CssBaseline />
