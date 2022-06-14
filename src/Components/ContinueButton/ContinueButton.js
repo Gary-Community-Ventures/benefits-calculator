7fca66015f62da3809094e76f4c435aa5552458d
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-14 15:27:39 -0600

    Refactor ContinueButton to useParams in order to capture id and pass it as an arg to handleSubmit

--- src/Components/ContinueButton/ContinueButton.js
+++ src/Components/ContinueButton/ContinueButton.js
@@ -1,11 +1,15 @@
 import { Button } from '@mui/material';
+import { useParams } from 'react-router-dom';
 
 const ContinueButton = ({ handleSubmit, inputError, formData, inputName }) => {
+  let { id } = useParams();
+  let numberId = Number(id);
+
   return (
     <Button
       variant='contained'
       onClick={(event) => {
-        handleSubmit(event, inputError, formData[inputName]);
+        handleSubmit(event, inputError, formData[inputName], numberId);
       }}>
       Continue
     </Button>
