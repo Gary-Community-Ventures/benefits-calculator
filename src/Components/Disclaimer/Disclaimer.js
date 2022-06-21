19ee88a44f26f39836f8ad9b0a775660a652b0c6
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-21 10:30:36 -0600

    Shorten disclaimer body by adding space

--- src/Components/Disclaimer/Disclaimer.js
+++ src/Components/Disclaimer/Disclaimer.js
@@ -24,7 +24,7 @@ const Disclaimer = ({ formData, handleCheckboxChange }) => {
 
   return (
     <div className='benefits-form'>
-      <p className='step-progress-title'>Step 1 of {questions.length + 1}</p>
+      <p className='step-progress-title'>Step 1 of { questions.length + 1 }</p>
       <h2 className='sub-header'>What you should know before we begin:</h2>
       <Card variant='outlined'>
         <CardContent>
@@ -33,8 +33,11 @@ const Disclaimer = ({ formData, handleCheckboxChange }) => {
           </Typography>
           <Typography variant='body1'>
             GCV's Benefit Calculator offers a screening tool that recommends public benefits programs for you to consider based upon information you provide about your household.
-            To enroll in a benefit, you need to apply through the appropriate administering agency. GCV's Benefit Calculator will tell you how to apply for any benefit on this website, but cannot guarantee eligibility. The administering agency will determine final eligibility upon your submission of an application for each program.
-            The information you share for purposes of public benefits screening will be anonymous and can be shared with City agencies to improve access to benefits and programs. To find out more about how the City might use the data you share with GCV's Benefit Calculator, visit the Terms of Use for [X link], and Privacy Policy for [Y link].
+            To enroll in a benefit, you need to apply through the appropriate administering agency. 
+            GCV's Benefit Calculator will tell you how to apply for any benefit on this website, but cannot guarantee eligibility. 
+            The administering agency will determine final eligibility upon your submission of an application for each program.
+            The information you share for purposes of public benefits screening will be anonymous and can be shared with City agencies to improve access to benefits and programs. 
+            To find out more about how the City might use the data you share with GCV's Benefit Calculator, visit the Terms of Use for [X link], and Privacy Policy for [Y link].
           </Typography>
         </CardContent>  
       </Card>
@@ -43,7 +46,7 @@ const Disclaimer = ({ formData, handleCheckboxChange }) => {
         gutterBottom >
         Check the box below and then click the button to get started.
       </Typography>
-      { buttonWasClicked && formData.agreeToTermsOfService === false && <StyledTypography>Please check the box below to continue.</StyledTypography> || <StyledTypography></StyledTypography> }
+      { buttonWasClicked && formData.agreeToTermsOfService === false && <StyledTypography> Please check the box below to continue. </StyledTypography> || <StyledTypography></StyledTypography> }
       <FormControlLabel 
         control={<Checkbox checked={formData.agreeToTermsOfService} onChange={handleCheckboxChange} />}  
         label='I have read, understand, and agree to the terms of: the GCV Disclaimer for Public Benefit Program Screening, the GCV Privacy Policy, and the GCV Benefits Calculator Terms of Use.'
