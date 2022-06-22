9f6c6adb2ba08e3241e9b61895c5afec1244ec14
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-22 16:03:39 -0600

    Add HouseholdDataBlock wireframe w age question

---
+++ src/Components/HouseholdDataBlock/HouseholdDataBlock.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import Textfield from '../Textfield/Textfield';
+import { ageHasError, displayAgeHelperText } from '../../Assets/validationFunctions';
+
+const HouseholdDataBlock = ({ formData }) => {
+  const { householdSize } = formData; //# of blocks that will need to be created for each household member
+  
+  const [householdData, setHouseholdData] = useState([{
+      age: '',
+      ageLabel: `Person 1 Age`
+    }
+  ]);
+
+  const createAgeQuestion = (personIndex) => {
+    const ageTextfieldProps = {
+      inputType: 'text',
+      inputName: 'age', 
+      inputValue: householdData[personIndex].age,
+      inputLabel: householdData[personIndex].ageLabel,
+      inputError: ageHasError,
+      inputHelperText: displayAgeHelperText
+    }
+
+    return (
+      <>
+        <p className='question-label'>How old are they?</p>
+        { createTextfield(ageTextfieldProps, personIndex) }
+        <p className='household-data-q-underline'></p>
+      </>
+    );
+  }
+
+  const createTextfield = (componentInputProps, index) => {
+    return (
+      <Textfield 
+        componentDetails={componentInputProps}
+        formData={householdData[index]}
+        handleTextfieldChange={handleTextfieldChange} 
+        index={index} />
+    );
+  }
+
+  const handleTextfieldChange = (event, index) => {
+    const { value } = event.target;
+    const numberUpToEightDigitsLongRegex = /^\d{0,8}$/;
+
+    if (numberUpToEightDigitsLongRegex.test(value)) {
+      const updatedHouseholdData = householdData.map((personData, i) => {
+        if (i === index) {
+          return {
+            age: value,
+            ageLabel: `Person ${i + 1} Age`
+          };
+        } else {
+          return personData;
+        }
+      });
+
+      setHouseholdData(updatedHouseholdData);
+    }
+  }
+
+  return (
+    <div className='person-data-container'>
+      { createAgeQuestion(0) }
+    </div>
+  );
+}
+
+export default HouseholdDataBlock;
