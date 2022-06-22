2d6b7e90b92dcda8e20b94d30b9cda84bb6f1e6c
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-22 16:36:48 -0600

    Add relationshipOptions.js file for hh relation dropdown menu

---
+++ src/Assets/relationshipOptions.js
@@ -0,0 +1,19 @@
+const relationshipOptions = {
+  child: 'Child',
+  fosterChild: 'Foster Child',
+  stepChild: 'Step-child',
+  grandChild: 'Grandchild',
+  spouse: 'Spouse',
+  parent: 'Parent',
+  fosterParent: 'Foster Parent',
+  stepParent: 'Step-parent',
+  grandParent: 'Grandparent',
+  sisterOrBrother: 'Sister/Brother',
+  stepSisterOrBrother: 'Step-sister/Step-brother',
+  boyfriendOrGirlfriend: 'Boyfriend/Girlfriend',
+  domesticPartner: 'Domestic Partner',
+  unrelated: 'Unrelated',
+  relatedOther: 'Related in some other way'
+};
+
+export default relationshipOptions;
