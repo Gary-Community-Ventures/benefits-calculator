4061dd9caaf7981e3db4c9309fe4b316cdf55b5e
Ivonne Hernandez <ivonne702@gmail.com>
2022-06-24 14:14:58 -0600

    Add a DropdownMenu component for relationship q

--- src/Components/DropdownMenu/DropdownMenu.js
+++ src/Components/DropdownMenu/DropdownMenu.js
@@ -1,16 +1,53 @@
-const DropdownMenu = (options) => {
+import { FormControl, Select, MenuItem, InputLabel } from "@mui/material";
+import { styled } from '@mui/material/styles';
+
+const StyledSelectfield = styled(Select)({
+  marginBottom: 20,
+  minWidth: 200
+});
+
+const DropdownMenu = ({ dropdownComponentProps, options, householdData, setHouseholdData, index }) => {
+  const { labelId, inputLabelText, id, label, disabledSelectMenuItemText } = dropdownComponentProps;
+
+  const handleSelectChange = (event) => {
+    const updatedHouseholdData = householdData.map((personData, i) => {
+      if (i === index) {
+        return {
+          ...personData,
+          relationshipToHH: event.target.value
+        };
+      } else {
+        return personData;
+      }
+    });
+
+    setHouseholdData(updatedHouseholdData);
+  }
+
+  const createMenuItems = (options) => {
+    const disabledSelectMenuItem = <MenuItem value='disabled-select' key='disabled-select' disabled>{disabledSelectMenuItemText}</MenuItem>
+    const menuItemKeys = Object.keys(options);
+    const menuItemLabels = Object.values(options);
+    
+    const dropdownMenuItems = menuItemKeys.map((option, i) => {
+      return (
+        <MenuItem value={option} key={option}>{menuItemLabels[i]}</MenuItem>
+      );
+    });
+    
+    return [disabledSelectMenuItem, dropdownMenuItems];
+  }
 
   return (
     <FormControl sx={{ m: 1, minWidth: 120 }}>
-      <InputLabel if='income-type-label'>Income Type</InputLabel>
+      <InputLabel id={labelId}>{inputLabelText}</InputLabel>
       <StyledSelectfield
-        labelId='income-type-label'
-        id={incomeStreamName}
-        value={incomeStreamName}
-        name={incomeStreamLabel}
-        label='Income Type'
-        onChange={(event) => { handleSelectChange(event, index) }}>
-        {createMenuItems()}
+        labelId={labelId}
+        id={id}
+        value={householdData[index].relationshipToHH}
+        label={label}
+        onChange={(event) => { handleSelectChange(event) }}>
+        {createMenuItems(options)}
       </StyledSelectfield>
     </FormControl>
   );
