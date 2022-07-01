4e5ad94108d2f230ec5a819dc1067097a817c824
Ivonne Hernandez <ivonne702@gmail.com>
2022-07-01 13:39:08 -0600

    Add HouseholdDataPreviousButton component

---
+++ src/Components/PreviousButton/HouseholdDataPreviousButton.js
@@ -0,0 +1,28 @@
+import { Button } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const HouseholdDataPreviousButton = ({ page, setPage }) => {
+  const navigate = useNavigate();
+
+  const handlePrevious = (event) => {
+    event.preventDefault();
+    
+    if (Number(page) === 0) { //we're at the first member's data block
+      //so we just want to navigate back to step 14
+      navigate('/step-14');
+    } else { 
+      //go back to the previous member's data block
+      setPage(Number(page) - 1);
+    }
+  }
+
+  return (
+    <Button
+      variant='contained'
+      onClick={(event) => { handlePrevious(event) }}>
+      Prev
+    </Button>
+  );
+}
+
+export default HouseholdDataPreviousButton;
