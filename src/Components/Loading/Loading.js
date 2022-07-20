1e799a7590958a739f2379a1f972f8298f53cd9c
Ivonne Hernandez <ivonne702@gmail.com>
2022-07-20 16:33:28 -0600

    Add a p tag with text to Loading.js

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -4,6 +4,7 @@ import './Loading.css';
 const Loading = () => {
   return (
     <div className='loading-container'>
+      <p className='loading-text'>Please wait while we gather your results...</p>
       <img 
         className='loading-image'
         src={loading}
