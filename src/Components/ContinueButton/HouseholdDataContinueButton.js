7258b4473913b112e1f3258cd6ced2f0ad85dfd6
Ivonne Hernandez <ivonne702@gmail.com>
2022-07-27 15:23:56 -0600

    Add householdMemberDataIsValid to HHDContBtn

--- src/Components/ContinueButton/HouseholdDataContinueButton.js
+++ src/Components/ContinueButton/HouseholdDataContinueButton.js
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material';
+import { householdMemberDataIsValid } from '../../Assets/validationFunctions';
 
-const HouseholdDataContinueButton = ({ page, setPage, householdSizeNumber, handleHouseholdDataSubmit, householdData }) => {
+const HouseholdDataContinueButton = ({ page, setPage, householdSizeNumber, handleHouseholdDataSubmit, setState, state }) => {
 
   const handleContinue = (event) => {
     event.preventDefault();
@@ -8,8 +9,10 @@ const HouseholdDataContinueButton = ({ page, setPage, householdSizeNumber, handl
     if ((Number(page) + 1) === (Number(householdSizeNumber) - 1)) {
       //then we're at the end and we want to send all of this data back to app via a function
       //we should first validate that all of the information has been filled out for each family member
-      //then we should call handleHouseholdDataSubmit, else display error msg to user
-      handleHouseholdDataSubmit(householdData);
+      //if that is the case we call handleHouseholdDataSubmit, else display error msg to user
+      if (householdMemberDataIsValid(state, setState)) {
+        handleHouseholdDataSubmit(state.householdData);
+      }
     } else { //there are still more household member block(s) that need to be filled out
       //we just want to validate that all of the information has been filled out
       //and then set the page + 1 for them to fill out the next member's information
