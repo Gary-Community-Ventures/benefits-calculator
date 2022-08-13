a2fd84b39587a24672d115b35a9026c3abca6061
Brian Hiatt <brian.hiatt@gmail.com>
2022-08-13 15:24:52 -0600

    Added basic styling for the results and email button using MUI Grid.

--- src/Components/EmailResults/EmailResults.js
+++ src/Components/EmailResults/EmailResults.js
@@ -3,6 +3,7 @@ import { styled } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import { emailHasError, displayEmailHelperText } from '../../Assets/validationFunctions';
 import { postUser, updateScreen } from '../../apiCalls';
+import Grid from '@mui/material/Grid';
 
 const StyledTextField = styled(TextField)({
   marginBottom: 20
@@ -29,35 +30,44 @@ const EmailResults = ({ formData, results, handleEmailTextfieldChange }) => {
 
   return (
     <main className='benefits-form'>
-       <h2 className='sub-header'>Email a copy of my results</h2>
-       <StyledTextField 
-        type='email'
-        name='email'
-        value={formData.email}
-        label='Email' 
-        onChange={(event) => {handleEmailTextfieldChange(event)}}
-        variant='outlined'
-        required
-        error={emailHasError(formData.email)}
-        helperText={displayEmailHelperText(formData.email)} 
-      />
-      <div className='question-buttons'>
-        <Button
-          variant='contained'
-          onClick={() => {
-            navigate('/results');
-          }}>
-          Prev
-        </Button>
-        <Button
-          variant='contained'
-          onClick={(event) => {
-            handleEmailSubmit(event);
-          }}>
-          Submit
-        </Button>
-        
-      </div>
+      <Grid container spacing={2}>
+        <Grid xs={12}>
+          <h2 className='sub-header'>Email a copy of my results</h2>
+        </Grid>
+        <Grid xs={12}>
+          <StyledTextField 
+            type='email'
+            name='email'
+            value={formData.email}
+            label='Email' 
+            onChange={(event) => {handleEmailTextfieldChange(event)}}
+            variant='outlined'
+            required
+            error={emailHasError(formData.email)}
+            helperText={displayEmailHelperText(formData.email)} 
+          />
+        </Grid>
+        <Grid xs={12}>
+          <div className='question-buttons'>
+            <Button
+              sx={{mr: 2}}
+              variant='contained'
+              onClick={() => {
+                navigate('/results');
+              }}>
+              Back
+            </Button>
+            <Button
+              variant='contained'
+              onClick={(event) => {
+                handleEmailSubmit(event);
+              }}>
+              Submit
+            </Button>
+            
+          </div>
+        </Grid>
+      </Grid>
     </main>
   );
 }
