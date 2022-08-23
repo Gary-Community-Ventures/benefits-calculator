b002d8d12339616f38576af00a815e3b5173d6bf
Ivonne Hernandez <ivonne702@gmail.com>
2022-08-23 12:19:50 -0600

    Add Wrapper to index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import Wrapper from '../src/Components/Wrapper/Wrapper';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Wrapper>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Wrapper>
 );
 
 // If you want to start measuring performance in your app, pass a function
