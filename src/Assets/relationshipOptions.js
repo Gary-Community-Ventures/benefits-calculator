e5c3c9ac0568f31edec7a6d9d14e7bcb73d8e636
Ivonne Hernandez <ivonne702@gmail.com>
2022-08-25 16:07:34 -0600

    Wrap all relationshipOptions properties in a FM

--- src/Assets/relationshipOptions.js
+++ src/Assets/relationshipOptions.js
@@ -1,19 +1,66 @@
+import { FormattedMessage } from 'react-intl';
+
 const relationshipOptions = {
-  child: 'Child',
-  fosterChild: 'Foster Child',
-  stepChild: 'Step-child',
-  grandChild: 'Grandchild',
-  spouse: 'Spouse',
-  parent: 'Parent',
-  fosterParent: 'Foster Parent',
-  stepParent: 'Step-parent',
-  grandParent: 'Grandparent',
-  sisterOrBrother: 'Sister/Brother',
-  stepSisterOrBrother: 'Step-sister/Step-brother',
-  boyfriendOrGirlfriend: 'Boyfriend/Girlfriend',
-  domesticPartner: 'Domestic Partner',
-  unrelated: 'Unrelated',
-  relatedOther: 'Related in some other way'
+  child: 
+    <FormattedMessage 
+      id='relationshipOptions.child' 
+      defaultMessage='Child' />,
+  fosterChild: 
+    <FormattedMessage 
+      id='relationshipOptions.fosterChild' 
+      defaultMessage='Foster Child' />,
+  stepChild: 
+    <FormattedMessage 
+      id='relationshipOptions.stepChild' 
+      defaultMessage='Step-child' />,
+  grandChild: 
+    <FormattedMessage 
+      id='relationshipOptions.grandChild' 
+      defaultMessage='Grandchild' />,
+  spouse: 
+    <FormattedMessage 
+      id='relationshipOptions.spouse' 
+      defaultMessage='Spouse' />,
+  parent: 
+    <FormattedMessage 
+      id='relationshipOptions.parent' 
+      defaultMessage='Parent' />,
+  fosterParent: 
+    <FormattedMessage 
+      id='relationshipOptions.fosterParent' 
+      defaultMessage='Foster Parent' />,
+  stepParent: 
+    <FormattedMessage 
+      id='relationshipOptions.stepParent' 
+      defaultMessage='Step-parent' />,
+  grandParent: 
+    <FormattedMessage 
+      id='relationshipOptions.grandParent' 
+      defaultMessage='Grandparent' />,
+  sisterOrBrother: 
+    <FormattedMessage 
+      id='relationshipOptions.sisterOrBrother' 
+      defaultMessage='Sister/Brother' />,
+  stepSisterOrBrother: 
+    <FormattedMessage 
+      id='relationshipOptions.stepSisterOrBrother' 
+      defaultMessage='Step-sister/Step-brother' />,
+  boyfriendOrGirlfriend: 
+    <FormattedMessage 
+      id='relationshipOptions.boyfriendOrGirlfriend' 
+      defaultMessage='Boyfriend/Girlfriend' />,
+  domesticPartner: 
+    <FormattedMessage 
+      id='relationshipOptions.domesticPartner' 
+      defaultMessage='Domestic Partner' />,
+  unrelated: 
+    <FormattedMessage 
+      id='relationshipOptions.unrelated' 
+      defaultMessage='Unrelated' />,
+  relatedOther: 
+    <FormattedMessage 
+      id='relationshipOptions.relatedOther' 
+      defaultMessage='Related in some other way' />
 };
 
 export default relationshipOptions;
