50d0cc5fe68f0efeaff2342353e71863d4f772b2
Ivonne Hernandez <ivonne702@gmail.com>
2022-08-26 11:00:16 -0600

    Wrap incomeOptions values in a FM

--- src/Assets/incomeOptions.js
+++ src/Assets/incomeOptions.js
@@ -1,24 +1,90 @@
+import { FormattedMessage } from 'react-intl';
+
 const incomeOptions = {
-  wages: 'Wages, salaries, tips',
-  selfEmployment: 'Self-employment Income',
-  unemployment: 'Unemployment Benefits',
-  cashAssistance: 'Cash Assistance Grant',
-  childSupport: 'Child Support (Received)',
-  disabilityMedicaid: 'Disability-related Medicaid',
-  sSI: 'Supplemental Security Income (SSI)',
-  sSDependent: 'Social Security Dependent Benefits',
-  sSDisability: 'Social Security Disability Benefits',
-  sSSurvivor: "Social Security Survivor's Benefits",
-  sSRetirement: 'Social Security Retirement Benefits',
-  cOSDisability: 'Colorado State Disability Benefits',
-  veteran: "Veteran's Pension or Benefits",
-  pension: 'Government or Private Pension',
-  deferredComp: 'Withdrawals from Deferred Compensation (IRA, Keogh, etc.)',
-  workersComp: "Worker's Compensation",
-  alimony: 'Alimony (Received)',
-  boarder: 'Boarder or Lodger',
-  gifts: 'Gifts/Contributions (Received)',
-  rental: 'Rental Income',
-  investment: 'Investment Income (interest, dividends, and profit from selling stocks)'
+  wages: 
+    <FormattedMessage 
+      id='incomeOptions.wages' 
+      defaultMessage='Wages, salaries, tips' />,
+  selfEmployment: 
+    <FormattedMessage 
+      id='incomeOptions.selfEmployment' 
+      defaultMessage='Self-employment Income' />,
+  unemployment: 
+    <FormattedMessage 
+      id='incomeOptions.unemployment' 
+      defaultMessage='Unemployment Benefits' />,
+  cashAssistance: 
+    <FormattedMessage 
+      id='incomeOptions.cashAssistance' 
+      defaultMessage='Cash Assistance Grant' />,
+  childSupport: 
+    <FormattedMessage 
+      id='incomeOptions.childSupport' 
+      defaultMessage='Child Support (Received)' />,
+  disabilityMedicaid: 
+    <FormattedMessage 
+      id='incomeOptions.disabilityMedicaid' 
+      defaultMessage='Disability-related Medicaid' />,
+  sSI: 
+    <FormattedMessage 
+      id='incomeOptions.sSI' 
+      defaultMessage='Supplemental Security Income (SSI)' />,
+  sSDependent: 
+    <FormattedMessage 
+      id='incomeOptions.sSDependent' 
+      defaultMessage='Social Security Dependent Benefits' />,
+  sSDisability: 
+    <FormattedMessage 
+      id='incomeOptions.sSDisability' 
+      defaultMessage='Social Security Disability Benefits' />,
+  sSSurvivor: 
+    <FormattedMessage 
+      id='incomeOptions.sSSurvivor' 
+      defaultMessage="Social Security Survivor's Benefits" />,
+  sSRetirement:
+    <FormattedMessage 
+      id='incomeOptions.sSRetirement' 
+      defaultMessage='Social Security Retirement Benefits' />,
+  cOSDisability: 
+    <FormattedMessage 
+      id='incomeOptions.cOSDisability' 
+      defaultMessage='Colorado State Disability Benefits' />,
+  veteran: 
+    <FormattedMessage 
+      id='incomeOptions.veteran' 
+      defaultMessage="Veteran's Pension or Benefits" />,
+  pension: 
+    <FormattedMessage 
+      id='incomeOptions.pension' 
+      defaultMessage='Government or Private Pension' />,
+  deferredComp: 
+    <FormattedMessage 
+      id='incomeOptions.deferredComp' 
+      defaultMessage='Withdrawals from Deferred Compensation (IRA, Keogh, etc.)' />,
+  workersComp: 
+    <FormattedMessage 
+      id='incomeOptions.workersComp' 
+      defaultMessage="Worker's Compensation" />,
+  alimony: 
+    <FormattedMessage 
+      id='incomeOptions.alimony' 
+      defaultMessage='Alimony (Received)' />,
+  boarder: 
+    <FormattedMessage 
+      id='incomeOptions.boarder' 
+      defaultMessage='Boarder or Lodger' />,
+  gifts: 
+    <FormattedMessage 
+      id='incomeOptions.gifts' 
+      defaultMessage='Gifts/Contributions (Received)' />,
+  rental: 
+    <FormattedMessage 
+      id='incomeOptions.rental' 
+      defaultMessage='Rental Income' />,
+  investment: 
+    <FormattedMessage 
+      id='incomeOptions.investment' 
+      defaultMessage='Investment Income (interest, dividends, and profit from selling stocks)' />
 }
+
 export default incomeOptions;
