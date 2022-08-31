d3406562b5046f36ead3184bef8605d08d793218
Ivonne Hernandez <ivonne702@gmail.com>
2022-08-31 14:24:04 -0600

    Wrap createExpenseDropdownMenu input and select label in FM for PEB

--- src/Components/ExpenseBlock/PersonExpenseBlock.js
+++ src/Components/ExpenseBlock/PersonExpenseBlock.js
@@ -103,13 +103,21 @@ const PersonExpenseBlock = ({ personData, state, setState, personDataIndex }) =>
   const createExpenseDropdownMenu = (expenseSourceName, expenseSourceLabel, index) => {
     return (
       <FormControl sx={{ m: 1, minWidth: 120 }}>
-        <InputLabel if='expense-type-label'>Expense Type</InputLabel>
+        <InputLabel if='expense-type-label'>
+          <FormattedMessage 
+            id='personExpenseBlock.createExpenseDropdownMenu-expenseTypeInputLabel' 
+            defaultMessage='Expense Type' />
+        </InputLabel>
         <StyledSelectfield
           labelId='expense-type-label'
           id={expenseSourceName}
           value={expenseSourceName}
           name={expenseSourceLabel}
-          label='Expense Type'
+          label={
+            <FormattedMessage 
+              id='personExpenseBlock.createExpenseDropdownMenu-expenseTypeInputLabel' 
+              defaultMessage='Expense Type' />
+          }
           onChange={(event) => { handleSelectChange(event, index) }}>
           {createExpenseMenuItems()}
         </StyledSelectfield>
