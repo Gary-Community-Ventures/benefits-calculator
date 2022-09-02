3e84c8af281157d49bd26f3afb595fcf09ad608c
Ivonne Hernandez <ivonne702@gmail.com>
2022-09-02 11:01:26 -0600

    Consolidate Results.js imports

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -1,8 +1,6 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { Fragment } from 'react';
+import { useEffect, useState, Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Button, Link, Typography, Accordion, AccordionSummary, AccordionDetails } from "@mui/material";
+import { Button, Link, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import Grid from '@mui/material/Grid';
 import SendIcon from '@mui/icons-material/Send';
