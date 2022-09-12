87ccc2a74158fc0da5601efb6aa74b438d347bc8
Ivonne Hernandez <ivonne702@gmail.com>
2022-09-12 17:23:56 -0600

    Refactor handleSubmit func for btn to route 18 to confirm-info in App

--- src/App.js
+++ src/App.js
@@ -191,7 +191,7 @@ const App = () => {
     if (!validateInputFunction(inputToBeValidated)) {
       if (stepId === 14 && householdSize === 1) { //if you're on the householdSize q and the value is 1
         navigate(`/step-${stepId + 2}`); //skip question 15 and go to 16
-      } else if (stepId === 17) {
+      } else if (stepId === 18) {
         navigate('/confirm-information');
       } else { //you've indicated that you're householdSize is larger than 1
         navigate(`/step-${stepId + 1}`);
