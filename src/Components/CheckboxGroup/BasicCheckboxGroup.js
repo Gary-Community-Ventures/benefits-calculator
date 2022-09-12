5928e84657d73ba81cd156cbe54245ee983c32d5
Ivonne Hernandez <ivonne702@gmail.com>
2022-09-12 16:58:23 -0600

    Add BasicCheckboxGroup comp

---
+++ src/Components/CheckboxGroup/BasicCheckboxGroup.js
@@ -0,0 +1,56 @@
+import { FormControlLabel, FormGroup, Checkbox, FormControl } from '@mui/material';
+
+const BasicCheckboxGroup = ({ stateVariable, options, state, setState }) => {
+
+  const handleCheckboxChange = (event) => {
+    const { name } = event.target;
+
+    const currentStateVariableObj = { ...state[stateVariable] };
+
+    const currentOptions = Object.keys(currentStateVariableObj);
+
+    const updatedStateVariableObj = currentOptions.reduce((acc, key) => {
+      if (name === key) {
+         acc[key] = !(currentStateVariableObj[key]);
+      } else {
+        acc[key] = currentStateVariableObj[key];
+      }
+      return acc;
+
+    }, {});
+
+    setState({ ...state, [stateVariable]: updatedStateVariableObj });
+  }
+
+  const createFormControlLabels = () => {
+    const optionKeys = Object.keys(options);
+    
+    const formControlLabels = optionKeys.map((optionKey) => {
+      return (
+        <FormControlLabel 
+          control={
+            <Checkbox 
+              checked={state[stateVariable][optionKey]} 
+              onChange={handleCheckboxChange}
+              name={optionKey} />
+          } 
+          label={options[optionKey]}
+          key={optionKey}
+        />
+      );
+    });
+
+    return formControlLabels;
+  }
+
+  return (
+    <FormControl>
+      <FormGroup>
+        {createFormControlLabels()}
+      </FormGroup>
+    </FormControl>
+  );
+
+}
+
+export default BasicCheckboxGroup;
