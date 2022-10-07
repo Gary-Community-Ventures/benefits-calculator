48ceeb78717d0332971176ecb7335902c0fbe8dc
Ivonne Hernandez <ivonne702@gmail.com>
2022-10-07 15:22:19 -0600

    Refactor signUpFormHasError return to include no email && phone

--- src/Assets/validationFunctions.js
+++ src/Assets/validationFunctions.js
@@ -323,7 +323,7 @@ const signUpFormHasError = (props) => {
   const atLeastOneCheckboxSelectionWasMade = [sendResults, sendUpdates, sendOffers]
     .some(box => box === true);
 
-  return (emailHasError(email)) || (phoneHasError(phone)) || (!firstName) ||
+  return (emailHasError(email)) || (phoneHasError(phone)) || (!email && !phone) || (!firstName) ||
     (!lastName) || (atLeastOneCheckboxSelectionWasMade === false) || (commConsent === false);
 }
 
