a8abfe479b467c6706830cb456166fdd25a007c9
Ivonne Hernandez <ivonne702@gmail.com>
2022-10-13 14:38:45 -0600

    Add displayDisclosureSection function and section

--- src/Components/SignUp/SignUp.js
+++ src/Components/SignUp/SignUp.js
@@ -79,6 +79,51 @@ const SignUp = ({ formData, handleTextfieldChange }) => {
         index='0' />
     );
   }
+
+  const displayDisclosureSection = () => {
+    return (
+      <>
+        <Typography variant='body1' sx={{ mt: '1rem' }} style={{ fontWeight: 600 }}>
+          <FormattedMessage
+            id='emailResults.return-consentText'
+            defaultMessage="By filling out this form, you agree to future contact from Gary or our affiliates regarding your use of the benefits calculator or to offer additional programs that may be of interest to you and your family. Standard message and data costs may apply to these communications. You may opt out of receiving these communications at any time through the opt-out link in the communication." />
+        </Typography>
+        <FormControlLabel
+          sx={{mb: 1}}
+          control={
+            <Checkbox 
+              checked={formData.signUpInfo.commConsent} 
+              onChange={handleCheckboxChange}
+              name='commConsent' 
+              required />
+          }
+          label={
+            <div>
+              <FormattedMessage 
+                id='emailResults.return-consentCheck1' 
+                defaultMessage="I have read, understand, and agree to the terms of My Friend Ben's " />
+              <FormattedMessage
+                id='emailResults.return-consentCheck'
+                defaultMessage="{linkVal}"
+                values={{ 
+                  linkVal: <a 
+                  href={privacyLink}
+                  target="_blank"
+                  rel='noopener noreferrer'>
+                    <FormattedMessage
+                      id='emailResults.return-consentCheckLink'
+                      defaultMessage='data privacy policy' />
+                    </a>
+                }} />
+              <FormattedMessage 
+                id='emailResults.return-consentCheck4' 
+                defaultMessage=" and consent to contact." />
+            </div>
+          }
+        />
+      </>
+    );
+  }
   
   return (
     <>
@@ -91,6 +136,9 @@ const SignUp = ({ formData, handleTextfieldChange }) => {
       </Grid>
       <Grid xs={12} item>
         { createPhoneTextfield() }
+      </Grid>
+      <Grid xs={12} item marginBottom={'1rem'}>
+        { displayDisclosureSection() }
       </Grid>
     </>
   );
