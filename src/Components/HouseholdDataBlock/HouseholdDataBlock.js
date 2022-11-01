56e2152dfa5da2b53199f67d3e0ebe8a6d1a9519
Ivonne Hernandez <ivonne702@gmail.com>
2022-11-01 15:53:43 -0600

    Refactor hhdb.createHOfHRelationQuestion-relation

--- src/Components/HouseholdDataBlock/HouseholdDataBlock.js
+++ src/Components/HouseholdDataBlock/HouseholdDataBlock.js
@@ -176,7 +176,7 @@ const HouseholdDataBlock = ({ formData, handleHouseholdDataSubmit }) => {
         <p className='question-label'>
           <FormattedMessage
             id='householdDataBlock.createHOfHRelationQuestion-relation'
-            defaultMessage='What is this person’s relationship to the head of the household?' />
+            defaultMessage='What is this person’s relationship to you?' />
         </p>
         { createRelationshipDropdownMenu(index) }
         <p className='household-data-q-underline'></p>
