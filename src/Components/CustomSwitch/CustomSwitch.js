c1b3101aec6e82daa79235e336a94efbeff32e9d
Ivonne Hernandez <ivonne702@gmail.com>
2022-11-30 13:18:50 -0700

    Add CustomSwitch component

---
+++ src/Components/CustomSwitch/CustomSwitch.js
@@ -0,0 +1,26 @@
+import { alpha, styled } from '@mui/material/styles';
+import Switch from '@mui/material/Switch';
+
+const CustomBlueSwitch = styled(Switch)(({ theme }) => ({
+  '& .MuiSwitch-switchBase.Mui-checked': {
+    color: '#0096B0',
+    '&:hover': {
+      backgroundColor: alpha('#0096B0', theme.palette.action.hoverOpacity),
+    },
+  },
+  '& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track': {
+    backgroundColor: '#0096B0',
+  },
+}));
+
+const CustomSwitch = ({ handleCustomSwitchToggle }) => {
+  return (
+    <CustomBlueSwitch 
+      onChange={(event) => { 
+        handleCustomSwitchToggle(event);
+      }}
+    />
+  );
+}
+
+export default CustomSwitch;
