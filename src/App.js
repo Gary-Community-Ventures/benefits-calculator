526a302f546fa000376d77c413313b25ad8b44c0
Ivonne Hernandez <ivonne702@gmail.com>
2022-12-17 20:24:55 -0700

    Move dev form data to its own file

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import EmailResults2 from './Components/EmailResults/EmailResults2';
 import LandingPage from './Components/LandingPage/LandingPage';
 import styleOverrides from './Assets/styleOverrides';
 import './App.css';
+// import { createDevFormData } from './Assets/devFormData';
 
 const TRACKING_ID = "G-NZ9D1VLR0D";
 ReactGA.initialize(TRACKING_ID);
@@ -87,87 +88,7 @@ const App = () => {
   }
 
   const [formData, setFormData] = useState(getCurrentState());
-
-  // const [formData, setFormData] = useState({
-  //   isTest: true,
-  //   externalID: searchParams.get('externalid') ? searchParams.get('externalid') : null,
-  //   agreeToTermsOfService: false,
-  //   age: '33',
-  //   zipcode: '80204',
-  //   county: '',
-  //   startTime: new Date().toJSON(),
-  //   student: false,
-  //   studentFulltime: false,
-  //   pregnant: true,
-  //   unemployed: false,
-  //   unemployedWorkedInLast18Mos: true,
-  //   blindOrVisuallyImpaired: false,
-  //   disabled: false,
-  //   veteran: false,
-  //   hasIncome: true,
-  //   incomeStreams: [{
-  //     incomeStreamName: 'wages', 
-  //     incomeAmount: '29000',
-  //     incomeFrequency: 'yearly'
-  //   }],
-  //   hasExpenses: true,
-  //   expenses: [{
-  //     expenseSourceName: 'rent',
-  //     expenseAmount: '500',
-  //     expenseFrequency: 'monthly'
-  //   }],
-  //   householdSize: '2',
-  //   householdData: [{
-  //     age: '3',
-  //     relationshipToHH: `child`,
-  //     student: false,
-  //     studentFulltime: false,
-  //     pregnant: false,
-  //     unemployed: false,
-  //     unemployedWorkedInLast18Mos: false,
-  //     blindOrVisuallyImpaired: false,
-  //     disabled: false,
-  //     veteran: false,
-  //     medicaid: false,
-  //     disabilityRelatedMedicaid: false,
-  //     noneOfTheseApply: true,
-  //     hasIncome: false,
-  //     incomeStreams: [],
-  //     hasExpenses: false,
-  //     expenses: []
-  //   }],
-  //   householdAssets: '1000',
-  //   relationship: 'headOfHousehold',
-  //   lastTaxFilingYear: '2021',
-  //   benefits: {
-  //     tanf: true,
-  //     wic: true,
-  //     snap: false,
-  //     lifeline: false,
-  //     acp: false,
-  //     eitc: false,
-  //     coeitc: false,
-  //     nslp: false,
-  //     ctc: false,
-  //     medicaid: false,
-  //     rtdlive: false,
-  //     cccap: false,
-  //     mydenver: false,
-  //     chp: false,
-  //     leap: false
-  //   },
-  //   referralSource: 'gary',
-  //   otherSource: '',
-  //   signUpInfo: {
-  //     email: 'testabc@gmail.com',
-  //     phone: '',
-  //     firstName: 'Test',
-  //     lastName: 'Test',
-  //     sendOffers: true,
-  //     sendUpdates: false,
-  //     commConsent: true
-  //   }
-  // });
+  // const [formData, setFormData] = useState(createDevFormData(searchParams));
 
   const initialResults = {
     eligiblePrograms: [], 
---
+++ src/Assets/devFormData.js
@@ -0,0 +1,82 @@
+export const createDevFormData = (searchParams) => {
+  return {
+    isTest: true,
+    externalID: searchParams.get('externalid') ? searchParams.get('externalid') : null,
+    agreeToTermsOfService: false,
+    age: '33',
+    zipcode: '80204',
+    county: '',
+    startTime: new Date().toJSON(),
+    student: false,
+    studentFulltime: false,
+    pregnant: true,
+    unemployed: false,
+    unemployedWorkedInLast18Mos: true,
+    blindOrVisuallyImpaired: false,
+    disabled: false,
+    veteran: false,
+    hasIncome: true,
+    incomeStreams: [{
+      incomeStreamName: 'wages', 
+      incomeAmount: '29000',
+      incomeFrequency: 'yearly'
+    }],
+    hasExpenses: true,
+    expenses: [{
+      expenseSourceName: 'rent',
+      expenseAmount: '500',
+      expenseFrequency: 'monthly'
+    }],
+    householdSize: '2',
+    householdData: [{
+      age: '3',
+      relationshipToHH: `child`,
+      student: false,
+      studentFulltime: false,
+      pregnant: false,
+      unemployed: false,
+      unemployedWorkedInLast18Mos: false,
+      blindOrVisuallyImpaired: false,
+      disabled: false,
+      veteran: false,
+      medicaid: false,
+      disabilityRelatedMedicaid: false,
+      noneOfTheseApply: true,
+      hasIncome: false,
+      incomeStreams: [],
+      hasExpenses: false,
+      expenses: []
+    }],
+    householdAssets: '1000',
+    relationship: 'headOfHousehold',
+    lastTaxFilingYear: '2021',
+    benefits: {
+      tanf: true,
+      wic: true,
+      snap: false,
+      lifeline: false,
+      acp: false,
+      eitc: false,
+      coeitc: false,
+      nslp: false,
+      ctc: false,
+      medicaid: false,
+      rtdlive: false,
+      cccap: false,
+      mydenver: false,
+      chp: false,
+      leap: false
+    },
+    referralSource: 'gary',
+    otherSource: '',
+    signUpInfo: {
+      email: 'testabc@gmail.com',
+      phone: '',
+      firstName: 'Test',
+      lastName: 'Test',
+      sendOffers: true,
+      sendUpdates: false,
+      commConsent: true
+    }
+  };
+};
