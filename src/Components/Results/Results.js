8cd235b15004fe318b607ac09e4e3ae80e6ee133
Ivonne Hernandez <ivonne702@gmail.com>
2023-01-24 13:22:36 -0700

    Move formatPhoneNumber func on it's own, rm null rtn

--- src/Components/Results/Results.js
+++ src/Components/Results/Results.js
@@ -249,19 +249,15 @@ const Results = ({ results, setResults, formData, programSubset, passedOrFailedT
     }
   }
 
-  const displayNavigators = (navigators) => {
-    //https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
-    function formatPhoneNumber(phoneNumberString) {
-			const cleaned = ('' + phoneNumberString).replace(/\D/g, '');
-			const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
-			if (match) {
-				const intlCode = match[1] ? '+1 ' : '';
-				return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join(
-					''
-				);
-			}
-			return null;
-		}
+  const formatPhoneNumber = (phoneNumberString) => {
+    const cleaned = ('' + phoneNumberString).replace(/\D/g, '');
+    const match = cleaned.match(/^(1|)?(\d{3})(\d{3})(\d{4})$/);
+    if (match) {
+      const intlCode = match[1] ? '+1 ' : '';
+      return [intlCode, '(', match[2], ') ', match[3], '-', match[4]].join('');
+    }
+  }
+
     if (navigators.length) {
       return (
         <>
