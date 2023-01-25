aa17c3b0a229cc6aa3cc3109cb425df202117171
Ivonne Hernandez <ivonne702@gmail.com>
2023-01-25 11:32:09 -0700

    Add acuteConditionOptions file and props to EN/ES

---
+++ src/Assets/acuteConditionOptions.js
@@ -0,0 +1,18 @@
+import { FormattedMessage } from 'react-intl';
+
+const acuteConditionOptions = {
+  foodInsecurity: 
+    <FormattedMessage
+      id='householdConditionOptions.foodInsecurity'
+      defaultMessage='food insecurity' />,
+  mentalHealthIssues: 
+    <FormattedMessage
+      id='householdConditionOptions.mentalHealthIssues'
+      defaultMessage='mental health issues' />,
+  housingIssues: 
+    <FormattedMessage
+      id='householdConditionOptions.housingIssues'
+      defaultMessage='housing issues' />
+};
+
+export default acuteConditionOptions;
