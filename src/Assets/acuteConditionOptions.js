05bff07e796c83a04f48a55cb5e0998258974b87
Ivonne Hernandez <ivonne702@gmail.com>
2023-01-25 13:44:44 -0700

    Refactor & add properties to acuteConditionOptions

--- src/Assets/acuteConditionOptions.js
+++ src/Assets/acuteConditionOptions.js
@@ -1,18 +1,30 @@
 import { FormattedMessage } from 'react-intl';
 
 const acuteConditionOptions = {
-  foodInsecurity: 
-    <FormattedMessage
-      id='householdConditionOptions.foodInsecurity'
-      defaultMessage='food insecurity' />,
-  mentalHealthIssues: 
-    <FormattedMessage
-      id='householdConditionOptions.mentalHealthIssues'
-      defaultMessage='mental health issues' />,
-  housingIssues: 
-    <FormattedMessage
-      id='householdConditionOptions.housingIssues'
-      defaultMessage='housing issues' />
+  food:
+    <FormattedMessage 
+      id='acuteConditionOptions.food' 
+      defaultMessage='Access to food' />,
+  babySupplies:
+    <FormattedMessage 
+      id='acuteConditionOptions.babySupplies' 
+      defaultMessage='Access to free diapers and other baby supplies' />,
+  housing:
+    <FormattedMessage 
+      id='acuteConditionOptions.housing' 
+      defaultMessage='Emergency help paying mortgage, rent, or utilities because of financial hardship' />,
+  support:
+    <FormattedMessage 
+      id='acuteConditionOptions.support' 
+      defaultMessage="A challenge that you (or your child) have that you'd be interested in talking with someone outside your household about" />,
+  childDevelopment:
+    <FormattedMessage 
+      id='acuteConditionOptions.childDevelopment' 
+      defaultMessage="Concern about your baby's toddler's development" />,
+  loss:
+    <FormattedMessage 
+      id='acuteConditionOptions.loss' 
+      defaultMessage='Funeral, burial, or cremation costs' />
 };
 
 export default acuteConditionOptions;
