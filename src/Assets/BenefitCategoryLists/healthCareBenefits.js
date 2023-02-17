b812cda58b24ee55476202384f74bf1ca6acf78a
Ivonne Hernandez <ivonne702@gmail.com>
2023-02-17 16:10:15 -0700

    Add healthCareBenefits list and properties to EN/ES

---
+++ src/Assets/BenefitCategoryLists/healthCareBenefits.js
@@ -0,0 +1,21 @@
+import { FormattedMessage } from 'react-intl';
+
+const healthCareBenefits = {
+  reproductivehealth:
+    <FormattedMessage
+      id='healthCareBenefits.reproductivehealth'
+      defaultMessage='Free birth control (Reproductive Health Care Program)'
+    />,
+  familyplanning:
+    <FormattedMessage
+      id='healthCareBenefits.familyplanning'
+      defaultMessage='Free family planning benefits (Family Planning Services)'
+    />,
+  dentallowincseniors:
+    <FormattedMessage
+      id='healthCareBenefits.dentallowincseniors'
+      defaultMessage='Low-cost dental care for people 60 years of age or older (Colorado Dental Health Program for Low-Income Seniors)'
+    />
+};
+
+export default healthCareBenefits;
