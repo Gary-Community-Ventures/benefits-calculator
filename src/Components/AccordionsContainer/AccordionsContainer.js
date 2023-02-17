6916137f324602815862293d37be07745ed744dc
Ivonne Hernandez <ivonne702@gmail.com>
2023-02-17 10:15:26 -0700

    WIP AccordionsContainer

---
+++ src/Components/AccordionsContainer/AccordionsContainer.js
@@ -0,0 +1,8 @@
+import benefitCategories from "../../Assets/benefitCategories";
+
+const AccordionsContainer = () => {
+  console.log({benefitCategories});
+  //need to pass a category and list to each accordion
+}
+
+export default AccordionsContainer;
