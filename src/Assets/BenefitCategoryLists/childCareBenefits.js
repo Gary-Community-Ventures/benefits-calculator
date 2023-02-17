af763a52755b528f07009aa4aa5b1b9d0eda6f75
Ivonne Hernandez <ivonne702@gmail.com>
2023-02-17 15:47:54 -0700

    Add childCareBenefits ls and properties to EN/ES

---
+++ src/Assets/BenefitCategoryLists/childCareBenefits.js
@@ -0,0 +1,31 @@
+import { FormattedMessage } from 'react-intl';
+
+const childCareBenefits = {
+  cccap:
+    <FormattedMessage
+      id='childCareBenefits.cccap'
+      defaultMessage='Help with child care costs (Colorado Child Care Assistance Program/CCCAP)'
+    />,
+  denverpresc:
+    <FormattedMessage
+      id='childCareBenefits.denverpresc'
+      defaultMessage='Tuition credits for Denver preschoolers (Denver Preschool Program)'
+    />,
+  coheadstart:
+    <FormattedMessage
+      id='childCareBenefits.coheadstart'
+      defaultMessage='Free early child care and preschool (Colorado Head Start)'
+    />,
+  univpresc:
+    <FormattedMessage
+      id='childCareBenefits.univpresc'
+      defaultMessage='Free preschool (Universal Preschool Colorado)'
+    />,
+  mydenver:
+    <FormattedMessage
+      id='childCareBenefits.mydenver'
+      defaultMessage='Reduced-cost youth programs (MY Denver Card)'
+    />
+};
+
+export default childCareBenefits;
