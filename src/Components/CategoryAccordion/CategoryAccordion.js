3174c4ca3acf538eb6e65c354e5590376bcf7654
Ivonne Hernandez <ivonne702@gmail.com>
2023-02-22 11:36:54 -0700

    Add accordionSummaryColor var and margin-bottom to CategoryAccordion

--- src/Components/CategoryAccordion/CategoryAccordion.js
+++ src/Components/CategoryAccordion/CategoryAccordion.js
@@ -6,13 +6,18 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import BasicCheckboxGroup from '../CheckboxGroup/BasicCheckboxGroup';
 
 const CategoryAccordion = ({ categoryName, categoryOptions, formData, setFormData, handleAccordionSelectChange, expanded, index }) => {
+  const accordionSummaryColor = (expanded === index) ? '#0096B0' : '#8d8d8d';
+
   return (
-    <Accordion expanded={expanded === index} onChange={handleAccordionSelectChange(index)}>
+    <Accordion
+      expanded={expanded === index}
+      onChange={handleAccordionSelectChange(index)}
+      sx={{ marginBottom: '.25rem' }}>
       <AccordionSummary
         expandIcon={<ExpandMoreIcon style={{ color: '#ffffff', height: '28.8' }}/>}
         aria-controls="panel1a-content"
         id="panel1a-header"
-        sx={{ backgroundColor: '#0096B0' }}
+        sx={{ backgroundColor: accordionSummaryColor }}
       >
         <Typography
           sx={{ color: '#ffffff', fontSize: '1.2rem', fontWeight: '400' }}>
