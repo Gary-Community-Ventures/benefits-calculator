df57f848e66f6c00d8fd9250fc6c4e361c2fdc87
Ivonne Hernandez <ivonne702@gmail.com>
2023-02-23 13:47:07 -0700

    Add allBenefitsList

---
+++ src/Assets/BenefitCategoryLists/allBenefitsList.js
@@ -0,0 +1,131 @@
+import { FormattedMessage } from 'react-intl';
+
+const allBenefitsList = {
+  tanf:
+    <FormattedMessage
+      id='cashAssistanceBenefits.tanf'
+      defaultMessage='Cash assistance and work support (Temporary Assistance for Needy Families (TANF/Colorado Works))'
+    />,
+  ssi:
+    <FormattedMessage
+      id='cashAssistanceBenefits.ssi'
+      defaultMessage='Federal cash assistance for individuals who are disabled, blind, or 65 years of age or older (Supplemental Security Income/SSI)'
+    />,
+  andcs:
+    <FormattedMessage
+      id='cashAssistanceBenefits.andcs'
+      defaultMessage='State cash assistance for individuals who are disabled and receiving SSI (Aid to the Needy Disabled - Colorado Supplement/AND-CS)'
+    />,
+  oap:
+    <FormattedMessage
+      id='cashAssistanceBenefits.oap'
+      defaultMessage='State cash assistance for individuals 60 years of age or older (Old Age Pension/OAP)'
+    />,
+  coPropTaxRentHeatCreditRebate:
+    <FormattedMessage
+      id='cashAssistanceBenefits.coPropTaxRentHeatCreditRebate'
+      defaultMessage='Cash to pay property tax, rent, and heat bills (Colorado Property Tax/Rent/Heat Credit Rebate)'
+    />,
+  cccap:
+    <FormattedMessage
+      id='childCareBenefits.cccap'
+      defaultMessage='Help with child care costs (Colorado Child Care Assistance Program/CCCAP)'
+    />,
+  denverpresc:
+    <FormattedMessage
+      id='childCareBenefits.denverpresc'
+      defaultMessage='Tuition credits for Denver preschoolers (Denver Preschool Program)'
+    />,
+  coheadstart:
+    <FormattedMessage
+      id='childCareBenefits.coheadstart'
+      defaultMessage='Free early child care and preschool (Colorado Head Start)'
+    />,
+  univpresc:
+    <FormattedMessage
+      id='childCareBenefits.univpresc'
+      defaultMessage='Free preschool (Universal Preschool Colorado)'
+    />,
+  mydenver:
+    <FormattedMessage
+      id='childCareBenefits.mydenver'
+      defaultMessage='Reduced-cost youth programs (MY Denver Card)'
+    />,
+  snap:
+    <FormattedMessage
+      id='foodAndNutritionBenefits.snap'
+      defaultMessage='Food assistance (Supplemental Nutrition Assistance Program/SNAP)'
+    />,
+  wic:
+    <FormattedMessage
+      id='foodAndNutritionBenefits.wic'
+      defaultMessage='Food and breastfeeding assistance (Special Supplemental Nutrition Program for Women, Infants, and Children/WIC)'
+    />,
+  nslp:
+    <FormattedMessage
+      id='foodAndNutritionBenefits.nslp'
+      defaultMessage='Free school meals (National School Lunch Program)'
+    />,
+  ede:
+    <FormattedMessage
+      id='foodAndNutritionBenefits.ede'
+      defaultMessage='Food support for people 60 years of age or older (Everyday Eats)'
+    />,
+  reproductivehealth:
+    <FormattedMessage
+      id='healthCareBenefits.reproductivehealth'
+      defaultMessage='Free birth control (Reproductive Health Care Program)'
+    />,
+  familyplanning:
+    <FormattedMessage
+      id='healthCareBenefits.familyplanning'
+      defaultMessage='Free family planning benefits (Family Planning Services)'
+    />,
+  dentallowincseniors:
+    <FormattedMessage
+      id='healthCareBenefits.dentallowincseniors'
+      defaultMessage='Low-cost dental care for people 60 years of age or older (Colorado Dental Health Program for Low-Income Seniors)'
+    />,
+  acp:
+    <FormattedMessage
+      id='housingAndUtilities.acp'
+      defaultMessage='Home internet discount (Affordable Connectivity Program)'
+    />,
+  lifeline:
+    <FormattedMessage
+      id='housingAndUtilities.lifeline'
+      defaultMessage='Phone or internet discount (Lifeline Phone/Internet Service)'
+    />,
+  leap:
+    <FormattedMessage
+      id='housingAndUtilities.leap'
+      defaultMessage='Help with winter heating bills (Low-Income Energy Assistance Program/LEAP)'
+    />,
+  erc:
+    <FormattedMessage
+      id='housingAndUtilities.erc'
+      defaultMessage='Free home energy upgrades (Energy Resource Center)'
+    />,
+  eitc:
+    <FormattedMessage
+      id='taxCreditBenefits.eitc'
+      defaultMessage='Federal tax credit: earned income (Earned Income Tax Credit)'
+    />,
+  coeitc:
+    <FormattedMessage
+      id='taxCreditBenefits.coeitc'
+      defaultMessage='State tax credit: earned income (Colorado Earned Income Tax Credit/Expanded Earned Income Tax Credit)'
+    />,
+  ctc:
+    <FormattedMessage
+      id='taxCreditBenefits.ctc'
+      defaultMessage='Federal tax credit: child tax credit (Child Tax Credit)'
+    />,
+  rtdlive:
+    <FormattedMessage
+      id='transportationBenefits.rtdlive'
+      defaultMessage='Discounted RTD fares (RTD LiVE)'
+    />
+}
+
+export default allBenefitsList;
