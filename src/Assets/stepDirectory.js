bd2614cff242ecabff27eee6d593bdabe6c8fb72
Ivonne Hernandez <ivonne702@gmail.com>
2023-02-27 14:22:25 -0700

    Add stepDirectory for easier nav/step refactoring

---
+++ src/Assets/stepDirectory.js
@@ -0,0 +1,23 @@
+const stepDirectory = {
+  age: '/step-2',
+  zipcode: '/step-3',
+  student: '/step-4',
+  pregnant: '/step-5',
+  unemployed: '/step-6',
+  blindOrVisuallyImpaired: '/step-7',
+  disabled: '/step-8',
+  veteran: '/step-9',
+  hasIncome: '/step-10',
+  hasExpenses: '/step-11',
+  acuteHHConditions: '/step-12',
+  hasBenefits: '/step-13',
+  healthInsurance: '/step-14',
+  householdSize: '/step-15',
+  householdData: '/step-16',
+  householdAssets: '/step-17',
+  lastTaxFilingYear: '/step-18',
+  referralSource: '/step-19',
+  signUpInfo: '/step-20'
+}
+
+export default stepDirectory;
