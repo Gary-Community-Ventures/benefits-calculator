475c9696c923e2196e8259c634e66f3ae61c1586
Ivonne Hernandez <ivonne702@gmail.com>
2023-03-08 13:14:29 -0700

    Add happy path test for Disclaimer

--- cypress/e2e/user_flow_input_test.cy.js
+++ cypress/e2e/user_flow_input_test.cy.js
@@ -1,7 +1,43 @@
-describe ('Userflow input test', () => {
-  it('As a user, when I visit the MFB landing page I should see the step progression display Step 0 of 21', () => {
+import stepDirectory from "../../src/Assets/stepDirectory";
+
+describe ('English user flow input test', () => {
+  it('When I visit the MFB landing page I should see the step progression display Step 0 of 21', () => {
     cy.visit('http://localhost:3000')
       .get('p[class="step-progress-title"]')
-      .should("contain", 'Step 0 of 21')
+      .should("contain", 'Step 0 of 21');
   });
+
+  it('When I visit the MFB landing page and click on the Continue button, it should navigate me to' +
+    ' Step 1 of the screener and display that progression in the url and progress counter', () => {
+    cy.visit('http://localhost:3000')
+      .get('button').should("contain", 'Continue').click()
+      .url()
+      .should("contain", 'http://localhost:3000/step-1')
+      .get('p[class="step-progress-title"]')
+      .should("contain", 'Step 1 of 21');
+
+  });
+
+  it('When I click on the Prev button in the Disclaimer' +
+    ' it should navigate me to back to Step 0 and that regression should be displayed in the url and progress counter', () => {
+    cy.visit('http://localhost:3000/step-1')
+      .get('button').should('have.class', 'prev-button').first().click()
+      .url()
+      .should("contain", 'http://localhost:3000/step-0')
+      .get('p[class="step-progress-title"]')
+      .should("contain", 'Step 0 of 21');
+  });
+
+  it('Happy path: When I click on the Disclaimer checkbox in step-1, and click on Continue' +
+    ' it should navigate me to Step 2 of the screener and display that progression in the url and progress counter', () => {
+    cy.visit('http://localhost:3000/step-1')
+      .get('input[type="checkbox"]').click()
+      .get('button').eq(1).click()
+      .url()
+      .should("contain", 'http://localhost:3000/step-2')
+      .get('p[class="step-progress-title"]')
+      .should("contain", 'Step 2 of 21');
+  });
+
+
 });
