ee12067a9e1e7926d8c59c87d9d515fd419847df
Ivonne Hernandez <ivonne702@gmail.com>
2023-03-22 13:38:39 -0600

    Add OptionCardGroup component

---
+++ src/Components/OptionCardGroup/OptionCardGroup.js
@@ -0,0 +1,62 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { CardActionArea, Typography } from '@mui/material';
+import './OptionCardGroup.css';
+
+const OptionCardGroup = ({ stateVariable, options, state, setState }) => {
+  const handleCardClick = (option) => {
+    const currentStateVariableObj = { ...state[stateVariable] };
+    const currentOptions = Object.keys(currentStateVariableObj);
+
+    const updatedStateVariableObj = currentOptions.reduce((acc, key) => {
+      if (option === key) {
+         acc[key] = !(currentStateVariableObj[key]);
+      } else {
+        acc[key] = currentStateVariableObj[key];
+      }
+      return acc;
+
+    }, {});
+
+    setState({ ...state, [stateVariable]: updatedStateVariableObj });
+  }
+
+  const createOptionCards = () => {
+    const optionCards = Object.keys(options).map((optionKey, index) => {
+      return (
+        <CardActionArea
+          key={index}
+        >
+          <Card
+            className={state[stateVariable][optionKey] ? 'selected-option-card' : 'unselected-option-card'}
+            sx={{ width: '179px', height: '174px', display: 'grid', placeItems: 'center' }}
+            onClick={() => {
+              handleCardClick(optionKey);
+            }}
+          >
+            <div className="option-card-image">
+              <img
+                src={options[optionKey].image}
+                alt={options[optionKey].formattedMessage}
+              />
+            </div>
+            <CardContent sx={{ textAlign: 'center', padding: '.35rem' }}>
+              <Typography>
+                {options[optionKey].formattedMessage}
+              </Typography>
+            </CardContent>
+          </Card>
+        </CardActionArea>
+      );
+    });
+
+    return optionCards;
+  }
+  return (
+    <div className="option-card-container">
+      { createOptionCards() }
+    </div>
+  );
+}
+
+export default OptionCardGroup;
