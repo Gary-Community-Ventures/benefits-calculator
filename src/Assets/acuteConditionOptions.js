35a321afd0992e417cffd2be4dc2196c98df1540
Ivonne Hernandez <ivonne702@gmail.com>
2023-03-22 14:33:12 -0600

    Refactor acuteCondOption values to objects with FM & image property

--- src/Assets/acuteConditionOptions.js
+++ src/Assets/acuteConditionOptions.js
@@ -1,30 +1,54 @@
 import { FormattedMessage } from 'react-intl';
+import cart from './OptionCardIcons/cart.png';
+import diaper from './OptionCardIcons/diaper.png';
+import home from './OptionCardIcons/home.png';
+import chat from './OptionCardIcons/chat.png';
+import stroller from './OptionCardIcons/stroller.png';
+import bill from './OptionCardIcons/bill.png';
 
 const acuteConditionOptions = {
-  food:
-    <FormattedMessage 
-      id='acuteConditionOptions.food' 
-      defaultMessage='Food or groceries' />,
-  babySupplies:
-    <FormattedMessage 
-      id='acuteConditionOptions.babySupplies' 
-      defaultMessage='Diapers and other baby supplies' />,
-  housing:
-    <FormattedMessage 
-      id='acuteConditionOptions.housing' 
-      defaultMessage='Emergency help paying mortgage, rent, or utilities because of financial hardship' />,
-  support:
-    <FormattedMessage 
-      id='acuteConditionOptions.support' 
-      defaultMessage="A challenge that you (or your child) have that you'd be interested in talking with someone outside your household about" />,
-  childDevelopment:
-    <FormattedMessage 
-      id='acuteConditionOptions.childDevelopment' 
-      defaultMessage="Concern about your baby or toddler's development" />,
-  loss:
-    <FormattedMessage 
-      id='acuteConditionOptions.loss' 
-      defaultMessage='Funeral, burial, or cremation costs' />
+  food: {
+    formattedMessage: <FormattedMessage
+      id='acuteConditionOptions.food'
+      defaultMessage='Food or groceries'
+    />,
+    image: cart
+  },
+  babySupplies: {
+    formattedMessage:  <FormattedMessage
+      id='acuteConditionOptions.babySupplies'
+      defaultMessage='Diapers and other baby supplies'
+    />,
+    image: diaper
+  },
+  housing: {
+    formattedMessage: <FormattedMessage
+      id='acuteConditionOptions.housing'
+      defaultMessage='Emergency help paying mortgage, rent, or utilities'
+    />,
+    image: home
+  },
+  support: {
+    formattedMessage: <FormattedMessage
+      id='acuteConditionOptions.support'
+      defaultMessage="A challenge you or your child would like to talk about"
+    />,
+    image: chat
+  },
+  childDevelopment: {
+    formattedMessage: <FormattedMessage
+      id='acuteConditionOptions.childDevelopment'
+      defaultMessage="Concern about your baby or toddler's development"
+    />,
+    image: stroller
+  },
+  loss: {
+    formattedMessage: <FormattedMessage
+      id='acuteConditionOptions.loss'
+      defaultMessage='Funeral, burial, or cremation costs'
+    />,
+    image: bill
+  }
 };
 
 export default acuteConditionOptions;
