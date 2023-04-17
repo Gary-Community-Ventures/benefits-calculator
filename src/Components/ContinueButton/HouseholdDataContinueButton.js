f3ba5e4dfdcd36fcde8736d07a0bc601dc5756e5
Ivonne Hernandez <ivonne702@gmail.com>
2023-04-17 16:05:02 -0600

    Add else if stmt to HHDB handleContinue to handle error, wasSubmitted state

--- src/Components/ContinueButton/HouseholdDataContinueButton.js
+++ src/Components/ContinueButton/HouseholdDataContinueButton.js
@@ -1,6 +1,6 @@
 import { Button } from '@mui/material';
 import { FormattedMessage } from 'react-intl';
-import { personDataIsValid } from '../../Assets/validationFunctions';
+import { getPersonDataErrorMsg, personDataIsValid } from '../../Assets/validationFunctions';
 import { useParams } from 'react-router-dom';
 
 const HouseholdDataContinueButton = ({ page, setPage, remainingHHMNumber, handleHouseholdDataSubmit, setState, state }) => {
@@ -9,15 +9,20 @@ const HouseholdDataContinueButton = ({ page, setPage, remainingHHMNumber, handle
 
   const handleContinue = (event) => {
     event.preventDefault();
-    setState({ ...state, error: '' }); //resets the error property
 
-    if (personDataIsValid(state, setState, page) && ((Number(page) + 1) === (Number(remainingHHMNumber))) ) {
+    if (personDataIsValid(state, page) && ((Number(page) + 1) === (Number(remainingHHMNumber)))) {
       //if this person's inputs are valid and we're at the last hh member then send the hhdata back up to App
       handleHouseholdDataSubmit(state.householdData, stepNumberId);
-    } else if ((personDataIsValid(state, setState, page))) { //we are not at the last page
-      //validate the householdMember's data if it's valid then 
+    } else if (personDataIsValid(state, page)) { //we are not at the last page
+      //validate the householdMember's data if it's valid then
       setPage(Number(page) + 1);
       window.scrollTo(0, 0);
+    } else if (!personDataIsValid(state, page)) {
+      setState({
+        ...state,
+        error: getPersonDataErrorMsg(state, page),
+        wasSubmitted: true
+      })
     }
   }
 
@@ -25,7 +30,7 @@ const HouseholdDataContinueButton = ({ page, setPage, remainingHHMNumber, handle
     <Button
       variant='contained'
       onClick={(event) => { handleContinue(event) }}>
-      <FormattedMessage 
+      <FormattedMessage
         id='continueButton'
         defaultMessage='Continue' />
     </Button>
