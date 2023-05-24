095dc6f1702f8ccb22947ae89bee80b3d128863b
Ivonne Hernandez <ivonne702@gmail.com>
2023-05-24 10:04:18 -0600

    Add eslint and prettier errors to test action

--- src/Components/Confirmation/Confirmation.js
+++ src/Components/Confirmation/Confirmation.js
@@ -172,7 +172,7 @@ const Confirmation = ({ formData }) => {
     const { householdData } = formData;
     const householdMembers = householdData.map((personData, index) => {
       if (index === 0) {
-        return <FormattedMessage id="qcc.hoh-text" defaultMessage="Head of household" key={index} />;
+        return <FormattedMessage id="qcc.hoh-text" defaultMessage="Head of household" />;
       } else {
         return relationshipOptions[personData.relationshipToHH];
       }
