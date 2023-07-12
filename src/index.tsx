52162a9eb729d01996f3a0b625f1ffd90a85ff51
Ivonne Hernandez <ivonne702@gmail.com>
2023-07-12 15:25:48 -0600

    Add .tsx to file to fix module not found err

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import Wrapper from './Components/Wrapper/Wrapper.tsx';
