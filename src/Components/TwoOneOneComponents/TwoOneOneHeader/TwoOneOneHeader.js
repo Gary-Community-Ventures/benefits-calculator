8cdd39e70a1a7b0697c724402b4aac133d5844c4
Ivonne Hernandez <ivonne702@gmail.com>
2023-07-24 13:35:47 -0600

    Add createAllHeaderLinks function

--- src/Components/TwoOneOneComponents/TwoOneOneHeader/TwoOneOneHeader.js
+++ src/Components/TwoOneOneComponents/TwoOneOneHeader/TwoOneOneHeader.js
@@ -1,9 +1,11 @@
 import { useContext } from 'react';
 import { useLocation } from 'react-router-dom';
-import { AppBar } from '@mui/material';
+import { AppBar, Link } from '@mui/material';
 import { Context } from '../../Wrapper/Wrapper';
 import twoOneOneMFBLogo from '../../../Assets/twoOneOneMFBLogo.png';
+import twoOneOneLinks from '../../../Assets/twoOneOneLinks';
 import './TwoOneOneHeader.css';
+import { FormattedMessage } from 'react-intl';
 
 const TwoOneOneHeader = () => {
   //this is so that when the users click on the cobranded logo, they're navigated back to step-0
@@ -18,12 +20,33 @@ const TwoOneOneHeader = () => {
   const isResults = url !== null;
   const screenUUID = url ? url[2] ?? url[1] : undefined;
 
+  const createAllHeaderLinks = () => {
+    const mappedLinks = twoOneOneLinks.map((link, index) => {
+      return (
+        <Link
+          href={link.href}
+          underline="none"
+          target="_blank"
+          rel="noreferrer"
+          aria-label={link.ariaLabel}
+          key={link.defaultMsg + index}
+          className='twoOneOneMenuLink'
+        >
+          <FormattedMessage id={link.formattedMsgId} defaultMessage={link.name} />
+        </Link>
+      );
+    });
+
+    return mappedLinks;
+  };
+
   return (
     <nav>
       <AppBar position="sticky" id="nav-container" sx={{ flexDirection: 'row', backgroundColor: '#FFFFFF' }}>
         <a href={`/step-0${urlSearchParams}`} className="home-link">
           <img src={twoOneOneMFBLogo} alt="211 and my friend ben home page button" className="logo cobranded-logo" />
         </a>
+        {createAllHeaderLinks()}
       </AppBar>
     </nav>
   );
