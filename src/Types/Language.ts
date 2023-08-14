1c997ef3398743d8dfa8ecca147dfb9088766433
Ivonne Hernandez <ivonne702@gmail.com>
2023-08-14 15:44:23 -0600

    Revert "Replace first bullet point, remove second bullet point"
    This reverts commit 4a421885a6f80f5b85bd93c1a82d3fdc49ecc408.

--- src/Types/Language.ts
+++ src/Types/Language.ts
@@ -365,6 +365,7 @@ export interface Language extends Record<string, string> {
   'landingPage.step-header': string;
   'landingPage.body': string;
   'landingPage.firstBulletItem': string;
+  'landingPage.secondBulletItem': string;
   'landingPage.publicCharge': string;
   'landingPage.publicChargeLink': string;
   'landingPage.thirdBulletItem': string;
