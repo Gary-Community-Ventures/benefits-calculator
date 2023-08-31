7a1940037580f035a0dc7158b770958d9d2e6e08
Ivonne Hernandez <ivonne702@gmail.com>
2023-08-31 10:40:40 -0600

    Add is13OrOlder property to FormData

--- src/Types/FormData.ts
+++ src/Types/FormData.ts
@@ -87,6 +87,7 @@ export interface FormData {
   isTest?: boolean;
   externalID?: string;
   agreeToTermsOfService: boolean;
+  is13OrOlder: boolean;
   zipcode: string;
   county: string;
   startTime: string;
