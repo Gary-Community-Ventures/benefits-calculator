3aed1c3e2fec8c8c891293dfe327ce9787f8c50c
Ivonne Hernandez <ivonne702@gmail.com>
2023-09-25 14:30:41 -0600

    Add select lang dropdown menu

--- src/Components/SelectLanguagePage/SelectLanguagePage.tsx
+++ src/Components/SelectLanguagePage/SelectLanguagePage.tsx
@@ -1,42 +1,50 @@
 
-import { languageSelectHasError, displayLanguageHasErrorText, useErrorController } from "../../Assets/validationFunctions";
+import { FormControl, Select, InputLabel, MenuItem, SelectChangeEvent } from "@mui/material";
+// import { languageSelectHasError, displayLanguageHasErrorText, useErrorController } from "../../Assets/validationFunctions.tsx";
 import { FormattedMessage } from "react-intl";
 import languageOptions from '../../Assets/languageOptions.tsx';
-import BasicSelect from "../DropdownMenu/BasicSelect.tsx";
+import { Context } from "../Wrapper/Wrapper.tsx";
+import { useContext } from "react";
 
 const SelectLanguagePage = () => {
-  const componentDetails = {
-    componentType: 'BasicSelect',
-    inputName: 'language',
-    inputError: languageSelectHasError,
-    inputHelperText: displayLanguageHasErrorText,
-    componentProperties: {
-      labelId: 'language-select',
-      inputLabelText: <FormattedMessage id="selectLang.text" defaultMessage="Language" />,
-      id: 'language-select',
-      value: 'language',
-      label: <FormattedMessage id="selectLang.text" defaultMessage="Language" />,
-      disabledSelectMenuItemText: (
-        <FormattedMessage
-          id="selectLang.disabledSelectMenuItemText"
-          defaultMessage="Select a language"
-        />
-      ),
-    },
-    options: languageOptions,
+  const context = useContext(Context);
+
+  const createMenuItems = (optionList:Record<string, string>, disabledFMId: string, disabledFMDefault: string) => {
+    const disabledSelectMenuItem = (
+      <MenuItem value="disabled-select" key="disabled-select" disabled>
+        <FormattedMessage id={disabledFMId} defaultMessage={disabledFMDefault} />
+      </MenuItem>
+    );
+    const menuItemKeys = Object.keys(optionList);
+    const menuItemLabels = Object.values(optionList);
+
+    const dropdownMenuItems = menuItemKeys.map((option, i) => {
+      return (
+        <MenuItem value={option} key={option}>
+          {menuItemLabels[i]}
+        </MenuItem>
+      );
+    });
+
+    return [disabledSelectMenuItem, dropdownMenuItems];
   };
 
-  const errorController = useErrorController(languageSelectHasError, displayLanguageHasErrorText);
-
   return (
-    <BasicSelect
-      componentDetails={componentDetails}
-      options={languageOptions}
-      formDataProperty="language"
-      submitted={0}
-      errorController={errorController}
-    />
-  )
+    <FormControl>
+      <InputLabel id="language-select-label">
+        <FormattedMessage id="selectLang.text" defaultMessage="Language" />
+      </InputLabel>
+      <Select
+        labelId="language-select-label"
+        id="language-select"
+        value={context.locale}
+        label={<FormattedMessage id="selectLang.text" defaultMessage="Language" />}
+        onChange={(event: SelectChangeEvent<string>) => context.selectLanguage(event)}
+      >
+        {createMenuItems(languageOptions, 'selectLang.disabledSelectMenuItemText', 'Select a language')}
+      </Select>
+    </FormControl>
+  );
 }
 
 export default SelectLanguagePage;
