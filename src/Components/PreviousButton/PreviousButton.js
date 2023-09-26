ae01fe21e247d03e49949c3a72b7f79ce56acf66
Ivonne Hernandez <ivonne702@gmail.com>
2023-09-26 10:26:07 -0600

    Remove unused startingQuestionNumber import

--- src/Components/PreviousButton/PreviousButton.js
+++ src/Components/PreviousButton/PreviousButton.js
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { Context } from '../Wrapper/Wrapper.tsx';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-import { getStepNumber, startingQuestionNumber } from '../../Assets/stepDirectory';
+import { getStepNumber } from '../../Assets/stepDirectory';
 
 const PreviousButton = ({ navFunction }) => {
   const { formData } = useContext(Context);
