69557fdcdb41db6b5328acc43893493a52a6bcff
Ivonne Hernandez <ivonne702@gmail.com>
2023-10-11 16:00:26 -0600

    Update filter label text

--- src/Assets/citizenshipFilterFormControlLabels.tsx
+++ src/Assets/citizenshipFilterFormControlLabels.tsx
@@ -11,52 +11,36 @@ export type CitizenLabels =
   | 'gc_under19_pregnant_no5';
 
 const citizenshipFilterFormControlLabels = {
-  citizen: (
-    <FormattedMessage
-      id="citizenshipFCtrlLabel-citizen"
-      defaultMessage="Show benefits that require citizenship or lawful U.S. presence"
-    />
-  ),
   non_citizen: (
     <FormattedMessage
       id="citizenshipFCtrlLabel-non_citizen"
-      defaultMessage="Show benefits that do not require citizenship or lawful U.S. presence"
+      defaultMessage="Individuals without citizenship or lawful U.S. presence"
     />
   ),
-  green_card: (
-    <FormattedMessage
-      id="citizenshipFCtrlLabel-green_card"
-      defaultMessage="Show benefits available to green card holders"
-    />
-  ),
+  green_card: <FormattedMessage id="citizenshipFCtrlLabel-green_card" defaultMessage="Green card holders" />,
   refugee: (
     <FormattedMessage
       id="citizenshipFCtrlLabel-refugee"
-      defaultMessage="Show benefits available to refugees or asylees (special rules or waiting periods may apply)"
+      defaultMessage="Refugees or asylees (special rules or waiting periods may apply)"
     />
   ),
-  gc_5plus: (
-    <FormattedMessage
-      id="citizenshipFCtrlLabel-gc_5plus"
-      defaultMessage="Show benefits available to green card holders ONLY after 5+ years in the U.S."
-    />
-  ),
+  gc_5plus: <FormattedMessage id="citizenshipFCtrlLabel-gc_5plus" defaultMessage="ONLY after 5+ years in the U.S." />,
   gc_18plus_no5: (
     <FormattedMessage
       id="citizenshipFCtrlLabel-gc_18plus_no5"
-      defaultMessage="Show benefits available to green card holders 18 or older without a 5-year waiting period"
+      defaultMessage="18 or older without a 5-year waiting period"
     />
   ),
   gc_under18_no5: (
     <FormattedMessage
       id="citizenshipFCtrlLabel-gc_under18_no5"
-      defaultMessage="Show benefits available to green card holders younger than 18 without a 5-year waiting period"
+      defaultMessage="younger than 18 without a 5-year waiting period"
     />
   ),
   gc_under19_pregnant_no5: (
     <FormattedMessage
       id="citizenshipFCtrlLabel-gc_under19_pregnant_no5"
-      defaultMessage="Show health care benefits available to green card holders younger than 19 or pregnant without a 5-year waiting period"
+      defaultMessage="for health care benefits - younger than 19 or pregnant without a 5-year waiting period"
     />
   ),
 };
