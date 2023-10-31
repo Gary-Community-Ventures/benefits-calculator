e1f282cfb018078fe5095ac5d2e099c2f656bb49
Caleb <caleblpena@gmail.com>
2023-10-31 15:42:43 -0600

    remove language file

--- src/Types/WrapperContext.ts
+++ src/Types/WrapperContext.ts
@@ -1,10 +1,11 @@
 import { FormData } from './FormData';
 import { ITheme } from '../Assets/styleController';
 import { ReferrerData } from '../Components/Referrer/referrerHook';
+import { Language } from '../Assets/languageOptions';
 
 export interface WrapperContext {
-  locale: string;
-  setLocale: (locale: string) => void;
+  locale: Language;
+  setLocale: (locale: Language) => void;
   selectLanguage: (event: Event) => void;
   formData: FormData;
   setFormData: (formData: FormData) => void;
