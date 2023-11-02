0f5f1c43382bba9c6b94745411fe40fac1e88a02
Caleb <caleblpena@gmail.com>
2023-11-02 12:45:34 -0600

    make previous button ts

--- src/Components/PreviousButton/PreviousButton.tsx
+++ src/Components/PreviousButton/PreviousButton.tsx
@@ -3,19 +3,22 @@ import { useContext } from 'react';
 import { Context } from '../Wrapper/Wrapper.tsx';
 import { useNavigate, useParams } from 'react-router-dom';
 import { FormattedMessage } from 'react-intl';
-import { getStepNumber } from '../../Assets/stepDirectory';
+import { getStepNumber } from '../../Assets/stepDirectory.ts';
 
-const PreviousButton = ({ navFunction }) => {
+type Props = {
+  navFunction: () => void;
+};
+
+const PreviousButton = ({ navFunction }: Props) => {
   const { formData } = useContext(Context);
   const { id, uuid } = useParams();
   let stepNumberId = Number(id);
   if (!stepNumberId) stepNumberId = 1;
   const navigate = useNavigate();
   const defaultNavigate = () => {
-    if (+id === getStepNumber('householdData', formData.immutableReferrer) + 1) {
-      navigate(
-        `/${uuid}/step-${getStepNumber('householdData', formData.immutableReferrer)}/${formData.householdData.length}`,
-      );
+    const householdStep = getStepNumber('householdData', formData.immutableReferrer);
+    if (id && +id === householdStep + 1) {
+      navigate(`/${uuid}/step-${householdStep}/${formData.householdData.length}`);
       return;
     }
     navigate(`/${uuid}/step-${stepNumberId - 1}`);
