80e7fcd3a2753177bb220fca70db11c1a1d71143
Caleb <caleblpena@gmail.com>
2023-11-03 12:35:14 -0600

    add languages back

--- src/Assets/languageOptions.tsx
+++ src/Assets/languageOptions.tsx
@@ -1,10 +1,10 @@
-export type Language = 'en-us' | 'es' | 'vi'; // | 'fr' | 'am';
+export type Language = 'en-us' | 'es' | 'vi' | 'fr' | 'am';
 const languageOptions: Record<Language, string> = {
   'en-us': 'English',
   es: 'Español',
   vi: 'Tiếng Việt',
-  // fr: 'Française',
-  // am: 'አማርኛ',
+  fr: 'Française',
+  am: 'አማርኛ',
 };
 
 export default languageOptions;
