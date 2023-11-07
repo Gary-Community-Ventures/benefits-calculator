5f39c5bc08a17ed35d10bff3ecd1d83177cbdc65
Ivonne Hernandez <ivonne702@gmail.com>
2023-11-07 15:43:15 -0700

    Add pageTitleTags file

---
+++ src/Assets/pageTitleTags.tsx
@@ -0,0 +1,17 @@
+const pageTitleTags: Record<string, string> = {
+  'step-1': 'Preferred Language',
+  'step-2': 'Legal',
+  'step-3': 'Zip and County',
+  'step-4': 'Number of HH Members',
+  'step-5': 'Individual HH Member',
+  'step-6': 'Expenses',
+  'step-7': 'Assets',
+  'step-8': 'Existing Benefits',
+  'step-9': 'Near Term Help',
+  'step-10': 'Referral',
+  'step-11': 'Optional Sign Up',
+  'confirm-information': 'Confirmation',
+  results: 'Results',
+};
+
+export default pageTitleTags;
