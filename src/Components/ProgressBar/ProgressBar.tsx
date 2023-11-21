341cb39df9ddf283f157a09b84dafff4ea350ae1
Carlos Cano <carlos.cano2013@gmail.com>
2023-11-21 14:50:02 -0500

    Prettier changes

--- src/Components/ProgressBar/ProgressBar.tsx
+++ src/Components/ProgressBar/ProgressBar.tsx
@@ -14,19 +14,19 @@ const ProgressBar = ({ step }: ProgressBarProps) => {
   const { theme, formData } = useContext(Context);
   const totalSteps = getStepDirectory(formData.immutableReferrer).length + STARTING_QUESTION_NUMBER;
   const { id } = useParams();
-  
+
   let stepValue = step || id || 0;
   let progressPercentage: number = ((Number(stepValue) - 1) / totalSteps) * 100;
-  
+
   const progressBarStyles = {
-    marginBottom: '5px', 
+    marginBottom: '5px',
     backgroundColor: '#FBF9FC',
     border: '1px solid #B0B0B0',
     borderRadius: '500rem;',
     height: '1rem',
     boxShadow: 'inset -1px 1px 3px rgb(0 0 0 / 0.2)',
     '& .MuiLinearProgress-bar': {
-      background: '#D6743F', 
+      background: '#D6743F',
       borderRadius: '500rem;',
     },
   };
