868b9339f79fc93d0ddee02eab236e4d19f4dabe
Ivonne Hernandez <ivonne702@gmail.com>
2023-12-05 09:25:24 -0700

    Add OptionCardGroup2 component

--- src/Components/OptionCardGroup/OptionCardGroup2.tsx
+++ src/Components/OptionCardGroup/OptionCardGroup2.tsx
@@ -1,52 +1,36 @@
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import { CardActionArea, Typography, Stack, FormHelperText } from '@mui/material';
+import { CardActionArea, Typography, Stack } from '@mui/material';
 import { ErrorController } from '../../Types/ErrorController';
 import { HouseholdData } from '../../Types/FormData';
-import { FormattedMessage, useIntl } from 'react-intl';
-
+import { useIntl } from 'react-intl';
+import './OptionCardGroup.css';
 
 interface OptionCardGroupProps {
   options: any; //change?
   memberData: HouseholdData;
-  // setMemberData: (householdMemberData: HouseholdData) => void;
+  setMemberData: (householdMemberData: HouseholdData) => void;
   hhMemberIndex?: number;
-  youVsThemOptions: any; //change?
   handleCardClick: any; //change?
-  errorController: ErrorController;
+  errorController?: ErrorController;
 }
 const OptionCardGroup2 = ({
   options,
   memberData,
-  // setMemberData,
+  setMemberData,
   hhMemberIndex,
-  youVsThemOptions,
   handleCardClick,
   errorController,
 }: OptionCardGroupProps) => {
   const intl = useIntl();
 
-  const displayOptionCards = (
-    options: any,
-    memberData: HouseholdData,
-    hhMemberIndex: number,
-  ) => {
-
+  const displayOptionCards = (options: any, memberData: HouseholdData, hhMemberIndex: number) => {
     const optionCards = Object.keys(options).map((key, index) => {
       const optionKey = key as typeof options;
       let translatedAriaLabel = intl.formatMessage({
         id: options[optionKey].formattedMessage.props.id,
         defaultMessage: options[optionKey].formattedMessage.props.defaultMessage,
       });
-      console.log({hhMemberIndex})
-      // console.log(`youVsThemOptions.you`, youVsThemOptions.you.props.defaultMessage);
-      // console.log({translatedAriaLabel})
-          console.log(youVsThemOptions);
-
-      if (hhMemberIndex === 1 && index === 0) { //if it's the first optionCard
-        console.log(`in here`)
-        translatedAriaLabel = youVsThemOptions.you.props.defaultMessage;
-      }
 
       return (
         <CardActionArea
@@ -54,7 +38,7 @@ const OptionCardGroup2 = ({
           sx={{ width: '11.25rem' }}
           className="card-action-area"
           onClick={() => {
-            handleCardClick(optionKey, memberData);
+            handleCardClick(optionKey, memberData, setMemberData);
           }}
           onKeyDown={(event) => {
             if (event.key === 'Enter') {
@@ -63,7 +47,7 @@ const OptionCardGroup2 = ({
           }}
         >
           <Card
-            className={options[optionKey] ? 'selected-option-card' : 'unselected-option-card'}
+            className={memberData[optionKey] ? 'selected-option-card' : 'unselected-option-card'}
             sx={{ width: '11.25rem', height: '11.25rem', display: 'grid', placeItems: 'center' }}
           >
             <div className="option-card-image">
