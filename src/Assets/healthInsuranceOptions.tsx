2d7da702b6a285944ec401eb7b06cd00cad4ef53
Ivonne Hernandez <ivonne702@gmail.com>
2023-12-05 11:05:06 -0700

    Update healthInsuranceOptions

--- src/Assets/healthInsuranceOptions.tsx
+++ src/Assets/healthInsuranceOptions.tsx
@@ -18,58 +18,127 @@ export type HealthInsuranceOptions = {
 };
 
 const healthInsuranceOptions: HealthInsuranceOptions = {
-  none: {
-    formattedMessage: (
-      <FormattedMessage id="healthInsuranceOptions.none-they" defaultMessage="They do not have health insurance" />
-    ),
-    image: redX,
+  you: {
+    none: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.none-I" defaultMessage="I do not have health insurance" />
+      ),
+      image: redX,
+    },
+    employer: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.employer" defaultMessage="Employer-provided health insurance" />
+      ),
+      image: building,
+    },
+    private: {
+      formattedMessage: (
+        <FormattedMessage
+          id="healthInsuranceOptions.private"
+          defaultMessage="Private (non-employer) health insurance"
+        />
+      ),
+      image: padlock,
+    },
+    medicaid: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.medicaid" defaultMessage="Health First Colorado (Full Medicaid)" />
+      ),
+      image: bill,
+    },
+    medicare: {
+      formattedMessage: <FormattedMessage id="healthInsuranceOptions.medicare" defaultMessage="Medicare" />,
+      image: umbrella,
+    },
+    chp: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.chp" defaultMessage="Child Health Plan Plus (CHP+)" />
+      ),
+      image: teddyDoctor,
+    },
+    emergency_medicaid: {
+      formattedMessage: (
+        <FormattedMessage
+          id="healthInsuranceOptions.emergency_medicaid"
+          defaultMessage="Emergency Medicaid / Reproductive Health"
+        />
+      ),
+      image: emergencyMedicaid,
+    },
+    family_planning: {
+      formattedMessage: (
+        <FormattedMessage
+          id="healthInsuranceOptions.family_planning"
+          defaultMessage="Family Planning Limited Medicaid"
+        />
+      ),
+      image: familyPlanning,
+    },
+    dont_know: {
+      formattedMessage: <FormattedMessage id="healthInsuranceOptions.dont_know" defaultMessage="Don't Know" />,
+      image: dontKnow,
+    },
   },
-  employer: {
-    formattedMessage: (
-      <FormattedMessage id="healthInsuranceOptions.employer" defaultMessage="Employer-provided health insurance" />
-    ),
-    image: building,
-  },
-  private: {
-    formattedMessage: (
-      <FormattedMessage id="healthInsuranceOptions.private" defaultMessage="Private (non-employer) health insurance" />
-    ),
-    image: padlock,
-  },
-  medicaid: {
-    formattedMessage: (
-      <FormattedMessage id="healthInsuranceOptions.medicaid" defaultMessage="Health First Colorado (Full Medicaid)" />
-    ),
-    image: bill,
-  },
-  medicare: {
-    formattedMessage: <FormattedMessage id="healthInsuranceOptions.medicare" defaultMessage="Medicare" />,
-    image: umbrella,
-  },
-  chp: {
-    formattedMessage: (
-      <FormattedMessage id="healthInsuranceOptions.chp" defaultMessage="Child Health Plan Plus (CHP+)" />
-    ),
-    image: teddyDoctor,
-  },
-  emergency_medicaid: {
-    formattedMessage: (
-      <FormattedMessage
-        id="healthInsuranceOptions.emergency_medicaid"
-        defaultMessage="Emergency Medicaid / Reproductive Health"
-      />
-    ),
-    image: emergencyMedicaid,
-  },
-  family_planning: {
-    formattedMessage: (
-      <FormattedMessage id="healthInsuranceOptions.family_planning" defaultMessage="Family Planning Limited Medicaid" />
-    ),
-    image: familyPlanning,
-  },
-  dont_know: {
-    formattedMessage: <FormattedMessage id="healthInsuranceOptions.dont_know" defaultMessage="Don't Know" />,
-    image: dontKnow,
+  them: {
+    none: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.none-they" defaultMessage="They do not have health insurance" />
+      ),
+      image: redX,
+    },
+    employer: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.employer" defaultMessage="Employer-provided health insurance" />
+      ),
+      image: building,
+    },
+    private: {
+      formattedMessage: (
+        <FormattedMessage
+          id="healthInsuranceOptions.private"
+          defaultMessage="Private (non-employer) health insurance"
+        />
+      ),
+      image: padlock,
+    },
+    medicaid: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.medicaid" defaultMessage="Health First Colorado (Full Medicaid)" />
+      ),
+      image: bill,
+    },
+    medicare: {
+      formattedMessage: <FormattedMessage id="healthInsuranceOptions.medicare" defaultMessage="Medicare" />,
+      image: umbrella,
+    },
+    chp: {
+      formattedMessage: (
+        <FormattedMessage id="healthInsuranceOptions.chp" defaultMessage="Child Health Plan Plus (CHP+)" />
+      ),
+      image: teddyDoctor,
+    },
+    emergency_medicaid: {
+      formattedMessage: (
+        <FormattedMessage
+          id="healthInsuranceOptions.emergency_medicaid"
+          defaultMessage="Emergency Medicaid / Reproductive Health"
+        />
+      ),
+      image: emergencyMedicaid,
+    },
+    family_planning: {
+      formattedMessage: (
+        <FormattedMessage
+          id="healthInsuranceOptions.family_planning"
+          defaultMessage="Family Planning Limited Medicaid"
+        />
+      ),
+      image: familyPlanning,
+    },
+    dont_know: {
+      formattedMessage: <FormattedMessage id="healthInsuranceOptions.dont_know" defaultMessage="Don't Know" />,
+      image: dontKnow,
+    },
   },
 };
 
