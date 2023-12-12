1e6c8ebcc2c2216b6994fbf3ab09680907a7a825
Ivonne Hernandez <ivonne702@gmail.com>
2023-12-12 13:35:59 -0700

    Make all acuteCond icons a ReactComponent, change image key to icon

--- src/Assets/acuteConditionOptions.tsx
+++ src/Assets/acuteConditionOptions.tsx
@@ -1,24 +1,24 @@
 import { FormattedMessage } from 'react-intl';
-import baby_supplies from './OptionCardIcons/AcuteConditions/baby_supplies.svg';
-import child_development from './OptionCardIcons/AcuteConditions/child_development.svg';
-import dental_care from './OptionCardIcons/AcuteConditions/dental_care.svg';
-import family_planning from './OptionCardIcons/AcuteConditions/family_planning.svg';
-import food from './OptionCardIcons/AcuteConditions/food.svg';
-import housing from './OptionCardIcons/AcuteConditions/housing.svg';
-import job_resources from './OptionCardIcons/AcuteConditions/job_resources.svg';
-import legal_services from './OptionCardIcons/AcuteConditions/legal_services.svg';
-import support from './OptionCardIcons/AcuteConditions/support.svg';
+import { ReactComponent as Baby_supplies } from './OptionCardIcons/AcuteConditions/baby_supplies.svg';
+import { ReactComponent as Child_development } from './OptionCardIcons/AcuteConditions/child_development.svg';
+import { ReactComponent as Dental_care } from './OptionCardIcons/AcuteConditions/dental_care.svg';
+import { ReactComponent as Family_planning } from './OptionCardIcons/AcuteConditions/family_planning.svg';
+import { ReactComponent as Food } from './OptionCardIcons/AcuteConditions/food.svg';
+import { ReactComponent as Housing } from './OptionCardIcons/AcuteConditions/housing.svg';
+import { ReactComponent as Job_resources } from './OptionCardIcons/AcuteConditions/job_resources.svg';
+import { ReactComponent as Legal_services } from './OptionCardIcons/AcuteConditions/legal_services.svg';
+import { ReactComponent as Support } from './OptionCardIcons/AcuteConditions/support.svg';
 
 const acuteConditionOptions = {
   food: {
     formattedMessage: <FormattedMessage id="acuteConditionOptions.food" defaultMessage="Food or groceries" />,
-    image: food,
+    icon: <Food className="option-card-icon" />,
   },
   babySupplies: {
     formattedMessage: (
       <FormattedMessage id="acuteConditionOptions.babySupplies" defaultMessage="Diapers and other baby supplies" />
     ),
-    image: baby_supplies,
+    icon: <Baby_supplies className="option-card-icon" />,
   },
   housing: {
     formattedMessage: (
@@ -27,7 +27,7 @@ const acuteConditionOptions = {
         defaultMessage="Help with managing your mortgage, rent, or utilities"
       />
     ),
-    image: housing,
+    icon: <Housing className="option-card-icon" />,
   },
   support: {
     formattedMessage: (
@@ -36,7 +36,7 @@ const acuteConditionOptions = {
         defaultMessage="A challenge you or your child would like to talk about"
       />
     ),
-    image: support,
+    icon: <Support className="option-card-icon" />,
   },
   childDevelopment: {
     formattedMessage: (
@@ -45,21 +45,21 @@ const acuteConditionOptions = {
         defaultMessage="Concern about your baby or toddler's development"
       />
     ),
-    image: child_development,
+    icon: <Child_development className="option-card-icon" />,
   },
   familyPlanning: {
     formattedMessage: (
       <FormattedMessage id="acuteConditionOptions.familyPlanning" defaultMessage="Family planning or birth control" />
     ),
-    image: family_planning,
+    icon: <Family_planning className="option-card-icon" />,
   },
   jobResources: {
     formattedMessage: <FormattedMessage id="acuteConditionOptions.jobResources" defaultMessage="Finding a job" />,
-    image: job_resources,
+    icon: <Job_resources className="option-card-icon" />,
   },
   dentalCare: {
     formattedMessage: <FormattedMessage id="acuteConditionOptions.dentalCare" defaultMessage="Low-cost dental care" />,
-    image: dental_care,
+    icon: <Dental_care className="option-card-icon" />,
   },
   legalServices: {
     formattedMessage: (
@@ -68,7 +68,7 @@ const acuteConditionOptions = {
         defaultMessage="Free or low-cost help with civil legal needs"
       />
     ),
-    image: legal_services,
+    icon: <Legal_services className="option-card-icon" />,
   },
 };
 
