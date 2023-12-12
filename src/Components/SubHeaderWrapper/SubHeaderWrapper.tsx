66e101de3bfbd78938ef9e5b63a7e72041e15b36
Ivonne Hernandez <ivonne702@gmail.com>
2023-12-12 15:30:35 -0700

    Delete SubHeaderWrapper since it's just an empty div

--- src/Components/SubHeaderWrapper/SubHeaderWrapper.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-export const SubHeaderWrapper = (props: PropsWithChildren<{}>, key?: string) => {
-  return <div className="secondary-heading">{props.children}</div>;
-};
