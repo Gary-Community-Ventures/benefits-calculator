35f96d31aa0fd832027c32ac167b06d96eb584bb
ming <ming@codethedream.org>
2024-01-16 20:59:54 -0800

    setup basic category heading component

---
+++ src/Components/CategoryHeading/CategoryHeading.js
@@ -0,0 +1,42 @@
+import { Typography, Box } from '@mui/material';
+import { ReactComponent as Food } from './food.svg';
+import { ReactComponent as Housing } from './housing.svg';
+import { ReactComponent as HealthCare } from './healthcare.svg';
+import './CategoryHeading.css';
+
+const textStyle = {
+  fontFamily: 'Roboto Slab',
+  fontWeight: '700',
+  fontSize: '1.25rem',
+  color: '#B85A27',
+};
+
+const headingOptionsMappings = {
+  housing: { icon: Housing, name: 'Housing and Utilities' },
+  food: { icon: Food, name: 'Food Benefits' },
+  healthcare: { icon: HealthCare, name: 'Medical Benefits' },
+};
+
+const CategoryHeading = ({ iconKey, amount }) => {
+  const { icon: IconComponent, name } = headingOptionsMappings[iconKey];
+  return (
+    <Box className='category-heading-container'>
+      <Box display="flex" alignItems="center">
+        <div className="category-heading-icon">
+          <IconComponent />
+        </div>
+        <Typography style={textStyle}>
+          {name}
+        </Typography>
+      </Box>
+
+      <Box display="flex" alignItems="center" >
+        <Typography style={{ ...textStyle, fontWeight: 'normal' }}>
+          ${amount}/mo.
+        </Typography>
+      </Box>
+    </Box >
+  );
+};
+
+export default CategoryHeading;
