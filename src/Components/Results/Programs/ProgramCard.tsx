56a8c86be9a2da6b704ff7983b16c7b5cc133ff4
Carlos Cano <carlos.cano2013@gmail.com>
2024-01-19 16:43:01 -0500

    Added padding to parent grid container. Added long-term benefit cards responsive styling.

--- src/Components/Results/Programs/ProgramCard.tsx
+++ src/Components/Results/Programs/ProgramCard.tsx
@@ -1,5 +1,6 @@
 import { Link, useParams } from 'react-router-dom';
 import { Program } from '../../../Types/Results';
+import { FormattedMessage } from 'react-intl';
 import ResultsTranslate from '../Translate/Translate';
 
 type ProgramCardProps = {
@@ -9,10 +10,26 @@ type ProgramCardProps = {
 const ProgramCard = ({ program }: ProgramCardProps) => {
   const { uuid } = useParams();
 
+  const estimatedAppTime = program.estimated_application_time;
+  const estimatedSavings = program.estimated_value;
+
   return (
-    <div>
-      <ResultsTranslate translation={program.name} />
-      <Link to={`/${uuid}/results/benefits/${program.program_id}`}>Apply</Link>
+    <div className="result-program-container">
+      <div className="result-program-more-info">
+        <ResultsTranslate translation={program.name} />
+        <Link to={`/${uuid}/results/benefits/${program.program_id}`}>More Info</Link>
+      </div>
+      <hr />
+      <div className="result-program-details">
+        <FormattedMessage id="results.estimated_application_time" defaultMessage="Application time" />
+        <strong>
+          <ResultsTranslate translation={estimatedAppTime} />
+        </strong>
+      </div>
+      <div className="result-program-details">
+        <FormattedMessage id="results.estimated_application_time" defaultMessage="Estimated Savings:" />
+        <strong>{`$${estimatedSavings}/mo`}</strong>
+      </div>
     </div>
   );
 };
