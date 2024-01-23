879965673d248781d5b14bbe4917b70793f4d343
Ivonne Hernandez <ivonne702@gmail.com>
2024-01-23 12:56:51 -0700

    Add back to screener, sv results WIP buttons

--- src/Components/Results/Header/Header.tsx
+++ src/Components/Results/Header/Header.tsx
@@ -1,14 +1,33 @@
+import { FormattedMessage } from 'react-intl';
+import { useNavigate, useParams } from 'react-router-dom';
 import { useResultsContext } from '../Results';
+import LeftArrowIcon from '@mui/icons-material/KeyboardArrowLeft';
+import SaveIcon from '@mui/icons-material/Save';
+import '../../Results/Results.css';
 
 type ResultsHeaderProps = {
   type: 'program' | 'need';
 };
 
 const Buttons = () => {
+  const navigate = useNavigate();
+  const { uuid: screenerId } = useParams();
+
   return (
-    <div>
-      <button>back to screen</button>
-      <button>send results</button>
+    <div className='results-back-save-btn-container'>
+      <button
+        className="results-back-save-buttons"
+        onClick={() => {
+          navigate(`/${screenerId}/confirm-information`);
+        }}
+      >
+        <LeftArrowIcon />
+        <FormattedMessage id="results.back-to-screen-btn" defaultMessage="BACK TO SCREENER" />
+      </button>
+      <button className="results-back-save-buttons">
+        <FormattedMessage id="results.save-results-btn" defaultMessage="SAVE MY RESULTS" />
+        <SaveIcon className='save-icon'/>
+      </button>
     </div>
   );
 };
