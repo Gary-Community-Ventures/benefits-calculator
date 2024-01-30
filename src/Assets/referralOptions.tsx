00ba5bf5e374c39bcd9a5afb25d1278703c856ae
Ivonne Hernandez <ivonne702@gmail.com>
2024-01-30 13:39:42 -0700

    Add gac property to referralOptions, reorder them by str len

--- src/Assets/referralOptions.tsx
+++ src/Assets/referralOptions.tsx
@@ -3,17 +3,18 @@ import { FormattedMessage } from 'react-intl';
 const referralOptions = {
   '211co': '2-1-1 Colorado',
   cedp: 'Community Economic Defense Project (CEDP)',
-  cch: 'Colorado Coalition for the Homeless',
-  jeffcoHS: 'Jeffco Human Services',
-  bia: 'Benefits in Action',
-  villageExchange: 'Village Exchange',
-  frca: 'Family Resource Center Association',
   taxAssistanceSite: (
     <FormattedMessage
       id="referralOptions.taxAssistanceSite"
       defaultMessage="Tax Assistance Site (VITA/Tax Help Colorado)"
     />
   ),
+  cch: 'Colorado Coalition for the Homeless',
+  frca: 'Family Resource Center Association',
+  jeffcoHS: 'Jeffco Human Services',
+  gac: "Get Ahead Colorado",
+  bia: 'Benefits in Action',
+  villageExchange: 'Village Exchange',
   lgs: "Let's Get Set",
   testOrProspect: <FormattedMessage id="referralOptions.testOrProspect" defaultMessage="Test / Prospective Partner" />,
   searchEngine: <FormattedMessage id="referralOptions.searchEngine" defaultMessage="Google or other search engine" />,
