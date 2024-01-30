cc41aab43c782460f71c427cc3c632a1041abcd0
mdemenko1 <mariannademenko@gmail.com>
2024-01-30 11:36:38 -0500

    fixing lint issues

--- src/Components/HelpBubbleIcon/helpBubbleIcon.tsx
+++ src/Components/HelpBubbleIcon/helpBubbleIcon.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import HelpBubble from '../../Assets/helpBubble.svg';
 
-export function HelpBubbleIcon({ className}: { className?: string}) {
+export function HelpBubbleIcon({ className }: { className?: string }) {
   return (
-      <img className={className} alt="MyFriendBen Help Bubble Icon" src={HelpBubble}  
-      style={{ height: '20px', width: '20px' }} 
-      />
+    <img
+      className={className}
+      alt="MyFriendBen Help Bubble Icon"
+      src={HelpBubble}
+      style={{ height: '20px', width: '20px' }}
+    />
   );
 }
