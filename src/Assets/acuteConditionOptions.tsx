bee98d6c8c2e7c296adb329dcd5ba7c95a8c8101
Carlos Cano <carlos.cano2013@gmail.com>
2024-02-06 13:42:06 -0500

    Created cards for urgent needs. Created new object to map api need response with appropriate category headings and icons. Added appropriate styling.

--- src/Assets/acuteConditionOptions.tsx
+++ src/Assets/acuteConditionOptions.tsx
@@ -9,6 +9,55 @@ import { ReactComponent as Job_resources } from './OptionCardIcons/AcuteConditio
 import { ReactComponent as Legal_services } from './OptionCardIcons/AcuteConditions/legal_services.svg';
 import { ReactComponent as Support } from './OptionCardIcons/AcuteConditions/support.svg';
 
+export const acuteConditionResultMapping = {
+  food: {
+    default_message: 'Food or groceries',
+    api_default_message: 'Food or Groceries',
+    icon: <Food className="option-card-icon" />,
+  },
+  babySupplies: {
+    default_message: 'Baby Supplies',
+    api_default_message: 'Diapers and Other Baby Supplies',
+    icon: <Baby_supplies className="option-card-icon" />,
+  },
+  housing: {
+    default_message: 'Managing housing costs',
+    api_default_message: 'Help with managing your mortgage, rent, or utilities',
+    icon: <Housing className="option-card-icon" />,
+  },
+  support: {
+    default_message: 'Behavioral Health',
+    api_default_message:
+      'A challenge that you (or your child) have that you’d be interested in talking with someone outside your household about',
+    icon: <Support className="option-card-icon" />,
+  },
+  childDevelopment: {
+    default_message: "Child's Development",
+    api_default_message: 'Concern about your baby’s or toddler’s development',
+    icon: <Child_development className="option-card-icon" />,
+  },
+  familyPlanning: {
+    default_message: 'Family planning',
+    api_default_message: 'Family planning or birth control',
+    icon: <Family_planning className="option-card-icon" />,
+  },
+  jobResources: {
+    default_message: 'Job Training',
+    api_default_message: 'Finding a job',
+    icon: <Job_resources className="option-card-icon" />,
+  },
+  dentalCare: {
+    default_message: 'Low-cost dental care',
+    api_default_message: 'Low-cost dental care',
+    icon: <Dental_care className="option-card-icon" />,
+  },
+  legalServices: {
+    default_message: 'Civil legal needs',
+    api_default_message: 'Free or low-cost help with civil legal needs',
+    icon: <Legal_services className="option-card-icon" />,
+  },
+};
+
 const acuteConditionOptions = {
   food: {
     formattedMessage: <FormattedMessage id="acuteConditionOptions.food" defaultMessage="Food or groceries" />,
--- src/Components/Results/Needs/NeedCard.tsx
+++ src/Components/Results/Needs/NeedCard.tsx
@@ -1,14 +1,52 @@
+import { ReactElement } from 'react';
 import { UrgentNeed } from '../../../Types/Results';
 import ResultsTranslate from '../Translate/Translate';
+import { acuteConditionResultMapping } from '../../../Assets/acuteConditionOptions';
 
 type NeedsCardProps = {
   need: UrgentNeed;
 };
 
+interface ResponseObject {
+  icon: React.ReactNode | null;
+  defaultMessage: string;
+}
+
+const getIconAndDefaultMessage = (
+  messageType: string,
+  acuteConditionResultMapping: Record<string, any>,
+): ResponseObject => {
+  const matchingType = Object.keys(acuteConditionResultMapping).find(
+    (key) => acuteConditionResultMapping[key]?.api_default_message.toLowerCase() === messageType.toLowerCase(),
+  );
+
+  if (matchingType) {
+    const { icon, default_message } = acuteConditionResultMapping[matchingType];
+    return { icon, defaultMessage: default_message };
+  }
+
+  return { icon: null, defaultMessage: '' };
+};
+
 const NeedCard = ({ need }: NeedsCardProps) => {
+  const needType = need.type.default_message;
+  const urgentNeed = getIconAndDefaultMessage(needType, acuteConditionResultMapping);
+  const translateNeed = { default_message: urgentNeed.defaultMessage, label: urgentNeed.defaultMessage };
+  const needLink = need.link.default_message;
   return (
-    <div>
-      <ResultsTranslate translation={need.name} />
+    <div className="result-program-container">
+      <div className="result-resource-more-info">
+        {urgentNeed.icon}
+        <span>
+          <ResultsTranslate translation={translateNeed} />
+          <strong>
+            <ResultsTranslate translation={need.name} />
+          </strong>
+        </span>
+        <a className="resource-more-info" href={needLink} target="_blank" rel="noreferrer">
+          More Info
+        </a>
+      </div>
     </div>
   );
 };
