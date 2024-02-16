2ffcfe6603250c63acc3d0e2390acaa3916b09fa
ming <ming@codethedream.org>
2024-02-16 09:38:21 -0800

    Recursively handle nested config data

--- src/Components/Config/configHook.tsx
+++ src/Components/Config/configHook.tsx
@@ -1,14 +1,34 @@
 import { useEffect, useState } from 'react';
 import { configEndpoint, header } from '../../apiCalls';
-
 import { ConfigApiResponse } from '../../Types/Config';
 import { Config } from '../../Types/Config';
+import { FormattedMessage } from 'react-intl';
 
+// Recursive function
+function transformItem(item: any): any {
+  if (typeof item !== 'object' || item === null) {
+    return item;
+  }
+
+  if (item.hasOwnProperty('_label')) {
+    return <FormattedMessage id={item._label} defaultMessage={item._default_message} />;
+  }
+
+  const config: Config = {};
+  for (const key in item) {
+    if (item.hasOwnProperty(key)) {
+      config[key] = transformItem(item[key]);
+    }
+  }
+  return config;
+}
+
+// Main function with recursion
 function transformConfigResponse(configResponse: ConfigApiResponse[]): Config {
   const output: Config = {};
 
   configResponse.forEach((item) => {
-    output[item.name] = item.data;
+    output[item.name] = transformItem(item.data);
   });
 
   return output;
