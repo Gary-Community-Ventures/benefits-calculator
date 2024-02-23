a5299ebaf70ff72a846f117f222c8a6b1fdb1f53
Ivonne Hernandez <ivonne702@gmail.com>
2024-02-23 10:08:41 -0700

    Add visit website btn and state for accordion

--- src/Components/Results/Needs/NeedCard.tsx
+++ src/Components/Results/Needs/NeedCard.tsx
@@ -1,4 +1,5 @@
 import { FormattedMessage, useIntl } from 'react-intl';
+import { useState } from 'react';
 import { UrgentNeed } from '../../../Types/Results';
 import ResultsTranslate from '../Translate/Translate';
 import { acuteConditionResultMapping } from '../../../Assets/acuteConditionOptions';
@@ -31,27 +32,52 @@ const getIconAndDefaultMessage = (
 
 const NeedCard = ({ need }: NeedsCardProps) => {
   const intl = useIntl();
+  const [infoIsOpen, setInfoIsOpen] = useState(false);
 
   const needLink = need.link.default_message;
   const translatedLink = intl.formatMessage({ id: need.link.label, defaultMessage: needLink });
 
   const needType = need.type.default_message;
   const urgentNeed = getIconAndDefaultMessage(needType, acuteConditionResultMapping);
+  const translatedNeedDesc = intl.formatMessage({
+    id: need.description.label,
+    defaultMessage: need.description.default_message,
+  });
 
   return (
-    <div className="result-program-container">
-      <div className="result-resource-more-info">
-        {urgentNeed.icon}
-        <span>
-          <ResultsTranslate translation={need.type} />
-          <strong>
-            <ResultsTranslate translation={need.name} />
-          </strong>
-        </span>
-        <a className="resource-more-info" href={translatedLink} target="_blank">
+    <div className="need-card-container">
+      <div className="header-and-button-divider">
+        <div className="result-resource-more-info">
+          {urgentNeed.icon}
+          <span>
+            <ResultsTranslate translation={need.type} />
+            <strong>
+              <ResultsTranslate translation={need.name} />
+            </strong>
+          </span>
+        </div>
+        <button
+          className="more-info-btn"
+          onClick={() => {
+            setInfoIsOpen(!infoIsOpen);
+          }}
+        >
           <FormattedMessage id="more-info" defaultMessage="More Info" />
-        </a>
+        </button>
       </div>
+      {infoIsOpen && (
+        <>
+          <article className="need-desc-paragraph">{translatedNeedDesc}</article>
+          <a href={`tel:${need.phone_number}`} className="phone-number">
+            {need.phone_number && need.phone_number}
+          </a>
+          <div className="visit-website-btn-container">
+            <button onClick={() => window.open(translatedLink, '_blank')} className="visit-website-btn">
+              <FormattedMessage id="visit-website-btn" defaultMessage="Visit Website" />
+            </button>
+          </div>
+        </>
+      )}
     </div>
   );
 };
