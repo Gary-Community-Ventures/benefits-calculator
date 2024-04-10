7e2367a3e90b336eeaa40aee4fd4efb35d0e2003
mdemenko1 <mariannademenko@gmail.com>
2024-04-10 15:49:04 -0400

    Adjusting for the back end

--- src/Assets/frequencyOptions.tsx
+++ src/Assets/frequencyOptions.tsx
@@ -3,7 +3,7 @@ import { FormattedMessage } from 'react-intl';
 const frequencyOptions = {
   weekly: <FormattedMessage id="frequencyOptions.weekly" defaultMessage="every week" />,
   biweekly: <FormattedMessage id="frequencyOptions.biweekly" defaultMessage="every 2 weeks" />,
-  twiceMonth: <FormattedMessage id="frequencyOptions.twiceMonth" defaultMessage="twice a month" />,
+  semimonthly: <FormattedMessage id="frequencyOptions.semimonthly" defaultMessage="twice a month" />,
   monthly: <FormattedMessage id="frequencyOptions.monthly" defaultMessage="every month" />,
   hourly: <FormattedMessage id="frequencyOptions.hourly" defaultMessage="hourly" />,
 };
