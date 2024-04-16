f64dfedeb63290ca427379df535491731c151551
Ivonne Hernandez <ivonne702@gmail.com>
2024-04-16 10:51:24 -0600

    Remove nested css rule, replace with more specific one

--- src/Components/Results/ProgramPage/ProgramPage.tsx
+++ src/Components/Results/ProgramPage/ProgramPage.tsx
@@ -108,13 +108,17 @@ const ProgramPage = ({ program }: ProgramPageProps) => {
                     )}
                     {info.email.default_message && (
                       <>
-                        <a href={`mailto:${info.email}`}>
+                        <a href={`mailto:${info.email}`} className="email-link">
                           <ResultsTranslate translation={info.email} />
                         </a>
                         <br />
                       </>
                     )}
-                    {info.phone_number && <a href={`tel:${info.phone_number}`}>{info.phone_number}</a>}
+                    {info.phone_number && (
+                      <a href={`tel:${info.phone_number}`} className="phone-link">
+                        {info.phone_number}
+                      </a>
+                    )}
                   </div>
                 </li>
               ))}
