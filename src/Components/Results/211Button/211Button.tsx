abd89ca6e86d3685fa07fa1063e8d8cc705d73ff
Ivonne Hernandez <ivonne702@gmail.com>
2024-04-30 10:34:31 -0600

    Refactor 211 button text to More Help

--- src/Components/Results/211Button/211Button.tsx
+++ src/Components/Results/211Button/211Button.tsx
@@ -1,15 +1,19 @@
 import { FormattedMessage } from 'react-intl';
+import { useNavigate, useParams } from 'react-router-dom';
 import './211Button.css';
 
 const HelpButton = () => {
+  const navigate = useNavigate();
+  const { uuid } = useParams();
+
   return (
     <div className="help-text-for-211-button">
       <h2 className="text-center help-text-for-211-button-font">
         <FormattedMessage id="moreHelp.211-header" defaultMessage="Can't find what you need?" />
       </h2>
-      <a href="https://www.211colorado.org/" className="button211" target="_blank" aria-label="More Help at 2-1-1">
-        <FormattedMessage id="moreHelp.211-link" defaultMessage="More Help at 2-1-1" />
-      </a>
+      <button onClick={() => navigate(`/${uuid}/results/more-help`)} className="button211" aria-label="More Help">
+        <FormattedMessage id="moreHelp.211-link" defaultMessage="More Help" />
+      </button>
     </div>
   );
 };
