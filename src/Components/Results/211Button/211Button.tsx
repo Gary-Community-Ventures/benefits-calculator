dabed072da6531db828fae991407343062edc960
Ivonne Hernandez <ivonne702@gmail.com>
2024-05-02 10:46:43 -0600

    Make button a Link instead

--- src/Components/Results/211Button/211Button.tsx
+++ src/Components/Results/211Button/211Button.tsx
@@ -1,9 +1,9 @@
 import { FormattedMessage } from 'react-intl';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import './211Button.css';
 
 const HelpButton = () => {
-  const navigate = useNavigate();
   const { uuid } = useParams();
 
   return (
@@ -11,9 +11,9 @@ const HelpButton = () => {
       <h2 className="text-center help-text-for-211-button-font">
         <FormattedMessage id="moreHelp.211-header" defaultMessage="Can't find what you need?" />
       </h2>
-      <button onClick={() => navigate(`/${uuid}/results/more-help`)} className="button211" aria-label="More Help">
+      <Link to={`/${uuid}/results/more-help`} className="button211" aria-label="More Help">
         <FormattedMessage id="moreHelp.211-link" defaultMessage="More Help" />
-      </button>
+      </Link>
     </div>
   );
 };
