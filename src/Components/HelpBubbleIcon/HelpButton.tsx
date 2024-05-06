765da261d1ff7f8ad84e426e93246b11e4ae036b
Ivonne Hernandez <ivonne702@gmail.com>
2024-05-06 11:26:23 -0600

    Add translated aria-label to HelpButton

--- src/Components/HelpBubbleIcon/HelpButton.tsx
+++ src/Components/HelpBubbleIcon/HelpButton.tsx
@@ -1,18 +1,19 @@
 import { IconButton } from '@mui/material';
 import { useState } from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { ReactComponent as HelpBubble } from '../../Assets/icons/helpBubble.svg';
 
 const HelpButton = ({ className, helpText, helpId }: { className?: string; helpText: string; helpId: string }) => {
+  const intl = useIntl();
   const [showHelpText, setShowHelpText] = useState(false);
-
   const handleClick = () => {
     setShowHelpText((setShow) => !setShow);
   };
+  const translatedAriaLabel = intl.formatMessage({ id: 'helpButton.ariaText', defaultMessage: 'help button' });
 
   return (
     <>
-      <IconButton onClick={handleClick}>
+      <IconButton onClick={handleClick} aria-label={translatedAriaLabel}>
         <HelpBubble style={{ height: '20px', width: '20px' }} />
       </IconButton>
       <p className={`${className} question-description help-text`}>
