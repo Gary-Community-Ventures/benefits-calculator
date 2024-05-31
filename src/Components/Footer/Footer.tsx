7f0a487b803b6aa51210ba7a329be5fbb54a884b
Ivonne Hernandez <ivonne702@gmail.com>
2024-05-31 09:43:00 -0600

    Rm extraneous getReferrer variable, fix imports

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,14 +1,13 @@
 import Paper from '@mui/material/Paper';
 import Share from '../Share/Share';
 import { useContext } from 'react';
-import { Context, theme } from '../Wrapper/Wrapper';
+import { Context } from '../Wrapper/Wrapper';
 import { FormattedMessage } from 'react-intl';
 import './Footer.css';
 
 const Footer = () => {
   const context = useContext(Context);
   const { getReferrer, theme } = context;
-  const { getReferrer } = context;
 
   return (
     <footer>
