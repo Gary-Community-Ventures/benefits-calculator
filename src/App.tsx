c02bc98e0d83c74e4737f9463065f5cf115c5a86
Ivonne Hernandez <ivonne702@gmail.com>
2024-05-31 10:52:29 -0600

    Add colorado-benefits route to App

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ import { useErrorController } from './Assets/validationFunctions.tsx';
 import dataLayerPush from './Assets/analytics.ts';
 import pageTitleTags, { StepName } from './Assets/pageTitleTags.ts';
 import { isCustomTypedLocationState } from './Types/FormData.ts';
+import CurrentCOBenefits from './Components/CurrentCOBenefits/CurrentCOBenefits.tsx';
 import './App.css';
 LicenseInfo.setLicenseKey(process.env.REACT_APP_MUI_LICENSE_KEY + '=');
 
@@ -304,6 +305,7 @@ const App = () => {
           </Routes>
           <Routes>
             <Route path="/" element={<Navigate to={`/step-1${urlSearchParams}`} replace />} />
+            <Route path='/colorado-benefits' element={<CurrentCOBenefits />}/>
             <Route path="/jeffcohs" element={<JeffcoLandingPage referrer="jeffcoHS" />} />
             <Route path="/jeffcohscm" element={<JeffcoLandingPage referrer="jeffcoHSCM" />} />
             <Route path="/step-1" element={<SelectLanguagePage />} />
