0a6da030a29d38d2e56a14a261dac138624a34d9
Ivonne Hernandez <ivonne702@gmail.com>
2024-06-11 13:44:43 -0600

    Remove some comments

--- src/Types/ApiProgramCategories.ts
+++ src/Types/ApiProgramCategories.ts
@@ -44,17 +44,12 @@ const findClosestMatchingDefaultMessage = (defaultMessage: string): string => {
   // is the default message perfect?
   if (programTemplateCategories.includes(defaultMessage)) {
     return defaultMessage;
-  };
-
+  }
 
   const normalizedDefaultMessage = normalizeDefaultMessage(defaultMessage);
-  // if not, we normalize the programTemplate categories first
-  //then find where in that array it matches with the normalizedDefaultMessage
-  //and return the value at that
   const maybeTheRightCategory = programTemplateCategories.find((programTemplate) => {
     return normalizeDefaultMessage(programTemplate) === normalizedDefaultMessage;
   });
-
 
   // is the default message invalid because of whitespace, commas, apostrophes, or hyphens?
   if (maybeTheRightCategory) {
@@ -62,10 +57,12 @@ const findClosestMatchingDefaultMessage = (defaultMessage: string): string => {
   } else {
     // is the default message invalid because it uses different wordings
     // or does it have typos not related to whitespace, comma, apostrophe, or hyphen characters?
-    const maybeClosestMatchingCategory = alternateProgramTemplateCategories.find((programTemplate) => programTemplate.previousNormalizedValues.includes(normalizedDefaultMessage));
+    const maybeClosestMatchingCategory = alternateProgramTemplateCategories.find((programTemplate) =>
+      programTemplate.previousNormalizedValues.includes(normalizedDefaultMessage),
+    );
 
     if (!maybeClosestMatchingCategory) {
-      throw new Error(`CategoryName is undefined`)
+      throw new Error(`CategoryName is undefined`);
     }
     return maybeClosestMatchingCategory.correctValue;
   }
