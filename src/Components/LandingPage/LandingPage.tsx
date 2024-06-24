6814f59378d09e3b537757469a99dea2b3691e78
Brian Espinosa <bespinosa1931@gmail.com>
2024-06-24 10:24:29 -0500

    chnage co public chrage link to nc link

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -137,7 +137,7 @@ const LandingPage = ({ handleCheckboxChange }: LandingPageProps) => {
               />
               <a
                 className="link-color"
-                href="https://cdhs.colorado.gov/public-charge-rule-and-colorado-immigrants#:~:text=About%20public%20charge&text=The%20test%20looks%20at%20whether,affidavit%20of%20support%20or%20contract."
+                href="https://www.ncjustice.org/publications/public-charge-the-law-has-changed/"
                 target="_blank"
                 onClick={() => {
                   dataLayerPush({
