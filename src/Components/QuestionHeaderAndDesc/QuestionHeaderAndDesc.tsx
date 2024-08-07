6b29be04b59cf9dc3f0bc13553719c29aa1f92dd
Ivonne Hernandez <ivonne702@gmail.com>
2024-08-07 10:21:31 -0600

    Add a QuestionHeaderAndDesc component

---
+++ src/Components/QuestionHeaderAndDesc/QuestionHeaderAndDesc.tsx
@@ -0,0 +1,17 @@
+import { FormattedMessageType } from "../../Types/Questions";
+
+type QuestionHeaderAndDescProps = {
+  question?: FormattedMessageType;
+  description?: FormattedMessageType;
+};
+
+const QuestionHeaderAndDesc = ({ question, description }: QuestionHeaderAndDescProps) => {
+  return (
+    <>
+      <h2 className="question-label">{question}</h2>
+      {description && <p className="question-description">{description}</p>}
+    </>
+  );
+}
+
+export default QuestionHeaderAndDesc;
