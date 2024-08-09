9cdb9fc3ef50b903c51716cd9ea1bf33f43db720
Brian Espinosa <bespinosa1931@gmail.com>
2024-08-09 16:10:37 -0500

    type udates to useLogo hook

--- src/Components/Referrer/useLogo.tsx
+++ src/Components/Referrer/useLogo.tsx
@@ -2,13 +2,14 @@ import { useContext } from 'react';
 import { Context } from '../Wrapper/Wrapper';
 import { MessageDescriptor, useIntl } from 'react-intl';
 import { LogoSource, renderLogoSource } from '../Referrer/referrerDataInfo';
+import { ReferrerData } from './referrerHook';
 
-export const useLogo = (className: string) => {
+export const useLogo = (src: keyof ReferrerData, alt: keyof ReferrerData, className: string) => {
   const { getReferrer } = useContext(Context);
   const intl = useIntl();
 
-  const logoSourceValue = getReferrer('logoSource') as LogoSource;
-  const logoAlt = getReferrer('logoAlt') as MessageDescriptor;
+  const logoSourceValue = getReferrer(src) as LogoSource;
+  const logoAlt = getReferrer(alt) as MessageDescriptor;
   
   const logo = renderLogoSource(logoSourceValue, intl.formatMessage(logoAlt), className);
 
