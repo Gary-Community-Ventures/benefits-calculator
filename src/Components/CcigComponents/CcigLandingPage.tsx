6bc3faa62c98b0440a6e80b497ccfc53fb88b707
Caleb <caleblpena@gmail.com>
2024-08-13 10:09:35 -0600

    update ccig text

--- src/Components/CcigComponents/CcigLandingPage.tsx
+++ src/Components/CcigComponents/CcigLandingPage.tsx
@@ -24,7 +24,36 @@ export default function CcigLandingPage() {
         })}
         className="ccig-landing-page-logo"
       />
-      <p className="ccig-landing-page-text">hello</p>
+      <p className="ccig-landing-page-text">
+        <FormattedMessage
+          id="ccig.landingPage.text.1"
+          defaultMessage="Welcome to the final part of the application process for the Colorado Community Insight Group!"
+        />
+      </p>
+      <p className="ccig-landing-page-text">
+        <FormattedMessage
+          id="ccig.landingPage.text.2"
+          defaultMessage="Thank you again for your interest in joining CCIG. This last section of the application will help us learn more about your family demographics, income, and benefits that you have experience with. This information is helpful to help match you to insight sessions to improve programs, initiatives, and policies."
+        />
+      </p>
+      <p className="ccig-landing-page-text">
+        <FormattedMessage
+          id="ccig.landingPage.text.3"
+          defaultMessage="Please take your time to fully complete all the questions this screener."
+        />
+      </p>
+      <p className="ccig-landing-page-text">
+        <FormattedMessage
+          id="ccig.landingPage.text.4"
+          defaultMessage="At the end of the screener, you will also learn about additional benefits and programs that you and your family could qualify for!"
+        />
+      </p>
+      <p className="ccig-landing-page-text">
+        <FormattedMessage
+          id="ccig.landingPage.text.5"
+          defaultMessage="If you have any questions or need assistance, please email us at ccig@garycommunity.org."
+        />
+      </p>
       <Button variant="contained" onClick={handleGetStarted}>
         <FormattedMessage id="jeffco-getStarted-button" defaultMessage="Get Started" />
       </Button>
--- src/Components/CcigComponents/CcigResultsMessage.tsx
+++ src/Components/CcigComponents/CcigResultsMessage.tsx
@@ -5,7 +5,7 @@ export default function CcigResultsMessage() {
     <div className="back-to-screen-message">
       <FormattedMessage
         id="ccig.results.message"
-        defaultMessage="Thanks for filling out MyFriendBen. You've completed you're CCIG registration. Review programs that you might be eligible for. . . something. . . @sydney-devine will sharpen this up later."
+        defaultMessage="Thank you for completing your application to join the Colorado Community Insight Group by filling out MyFriendBen. Your application will be processed within 2 - 4 days. In the meantime, we encourage you to explore the benefits and resources that may be available to your family listed below. If you have any questions in the meantime, please feel free to email us at ccig@garycommunity.org."
       />
     </div>
   );
