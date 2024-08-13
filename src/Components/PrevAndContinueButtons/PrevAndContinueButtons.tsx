d99c500ea8d65148cd5153d5b79a8ef35ed3d671
Ivonne Hernandez <ivonne702@gmail.com>
2024-08-13 14:57:32 -0600

    Add a PrevAndContinueButtons component to rp hardcoded on in zipcode q

---
+++ src/Components/PrevAndContinueButtons/PrevAndContinueButtons.tsx
@@ -0,0 +1,25 @@
+import { useNavigate, useParams } from "react-router-dom";
+import PreviousButton from "../PreviousButton/PreviousButton";
+import { Button } from "@mui/material";
+import { FormattedMessage } from "react-intl";
+
+type PrevAndContinueButtonsProps = {
+  currentStepId: number;
+};
+
+const PrevAndContinueButtons = ({ currentStepId }: PrevAndContinueButtonsProps) => {
+  const { uuid } = useParams();
+  const navigate = useNavigate();
+  const backNavigationFunction = () => navigate(`/${uuid}/step-${currentStepId - 1}`);
+
+  return (
+    <div className="question-buttons">
+      <PreviousButton navFunction={backNavigationFunction} />
+      <Button variant="contained" type="submit">
+        <FormattedMessage id="continueButton" defaultMessage="Continue" />
+      </Button>
+    </div>
+  );
+};
+
+export default PrevAndContinueButtons;
