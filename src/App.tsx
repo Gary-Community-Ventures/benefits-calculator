7714f04d611d02f74b62653624afc4683dd03f69
Ivonne Hernandez <ivonne702@gmail.com>
2024-08-15 10:06:00 -0600

    Move isTest and externalId logic inside a useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -32,9 +32,6 @@ const App = () => {
   const location = useLocation();
   const urlSearchParams = location.search;
   const [searchParams] = useSearchParams();
-  const isTest = searchParams.get('test') ? true : false;
-  const rawExternalId = searchParams.get('externalid');
-  const externalId = rawExternalId !== null ? rawExternalId : undefined;
   const {
     locale,
     formData,
@@ -135,6 +132,8 @@ const App = () => {
     }
     const referrerParam = searchParams.get('referrer');
     const utmParam = searchParams.get('utm_source');
+    const testParam = searchParams.get('test') ? true : false
+    const externalIdParam = searchParams.get('externalid');
 
     // referrer priority = stored referrer -> referrer param -> utm_source param -> ''
     const referrer = formData.immutableReferrer ?? referrerParam ?? utmParam ?? '';
@@ -143,6 +142,8 @@ const App = () => {
     if (referrerSource === 'other') {
       referrerSource = formData.otherSource ?? '';
     }
+    const isTest = formData.isTest || testParam;
+    const externalId = formData.externalID ?? externalIdParam ?? undefined;
 
     // if the referrer source is not in the dropdown list then
     // make the referrer "other" and fill in the other field
