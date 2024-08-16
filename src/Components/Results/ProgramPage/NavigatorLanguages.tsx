90648cce4fd073bb4569e8618a80f19aeb261f9f
Ivonne Hernandez <ivonne702@gmail.com>
2024-08-16 12:33:27 -0600

    Add NavigatorLanguages list

---
+++ src/Components/Results/ProgramPage/NavigatorLanguages.tsx
@@ -0,0 +1,15 @@
+import { FormattedMessage } from "react-intl";
+
+export const allNavigatorLanguages = {
+  'en-us': <FormattedMessage id="navLanguage.en-us" defaultMessage="English" />,
+  es: <FormattedMessage id="navLanguage.es" defaultMessage="Spanish" />,
+  vi: <FormattedMessage id="navLanguage.vi" defaultMessage="Vietnamese" />,
+  fr: <FormattedMessage id="navLanguage.fr" defaultMessage="French" />,
+  am: <FormattedMessage id="navLanguage.am" defaultMessage="Amharic" />,
+  so: <FormattedMessage id="navLanguage.so" defaultMessage="Somali" />,
+  ru: <FormattedMessage id="navLanguage.ru" defaultMessage="Russian" />,
+  ne: <FormattedMessage id="navLanguage.ne" defaultMessage="Nepali" />,
+  my: <FormattedMessage id="navLanguage.my" defaultMessage="Burmese" />,
+  zh: <FormattedMessage id="navLanguage.zh" defaultMessage="Chinese" />,
+  ar: <FormattedMessage id="navLanguage.ar" defaultMessage="Arabic" />,
+};
