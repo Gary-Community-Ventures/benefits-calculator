717d52eb27603afc14b26bf6d10b341129cfc8af
Marianna Demenko <65931890+Mdemenko1@users.noreply.github.com>
2024-09-11 12:59:08 -0400

    1196: adjusting modal opening

--- src/Components/FeedbackIcon/feedbackModal.tsx
+++ src/Components/FeedbackIcon/feedbackModal.tsx
@@ -1,41 +1,45 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './feedbackIcon.css';
 import { FormattedMessage } from 'react-intl';
 import '../../Components/Results/211Button/211Button.css';
+import { useConfig } from '../Config/configHook';
 
 interface FeedbackModalProps {
   handleCloseModal: () => void;
 }
 
 const FeedbackModal: React.FC<FeedbackModalProps> = ({ handleCloseModal }) => {
+  const { email, survey } = useConfig('feedback_links');
+
   const onEmailClick = () => {
     handleCloseModal();
-    window.location.href =
-      'mailto:ricky@codethedream.org?subject=Feedback&body=Please%20provide%20your%20feedback%20here.';
+    window.location.href = email;
   };
 
-  const surveyLink =
-    'https://docs.google.com/forms/d/e/1FAIpQLSdrwxIm8QOpaEDYH1UOahpzMIr3w6OSqqxjmEKCOoAZyWppeg/viewform';
+  const surveyLink = survey;
 
   return (
-    <div className="feedback-container">
-      <div className="feedback-header">
-        <FormattedMessage id="feedbackModal-header" defaultMessage="MyFriendBen Feedback" />
-      </div>
-      <FormattedMessage id="feedbackModal-question" defaultMessage="Tell us what you think" />
-      <p className="border-bottom"></p>
-      <div className="button-wrapper">
-        <button onClick={onEmailClick} className="button211">
-          <FormattedMessage id="emailButton" defaultMessage="Email" />
-        </button>
-        <button>
-          <a href={surveyLink} target="_blank" className="button211">
-            <FormattedMessage id="surveyButton" defaultMessage="Survey" />
-          </a>
-        </button>
-        <button onClick={handleCloseModal} className="close-button">
+    <div className="feedback-container-wrapper">
+      <div className="feedback-container">
+      
+        <div className="feedback-header">
+          <FormattedMessage id="feedbackModal-header" defaultMessage="MyFriendBen Feedback" />
+          <button onClick={handleCloseModal} className="close-button">
           ✕
         </button>
+        </div>
+        <FormattedMessage id="feedbackModal-question" defaultMessage="Tell us what you think" />
+        <p className="border-bottom"></p>
+        <div className="button-wrapper">
+          <button onClick={onEmailClick} className="button211">
+            <FormattedMessage id="emailButton" defaultMessage="Email" />
+          </button>
+          <button>
+            <a href={surveyLink} target="_blank" className="button211">
+              <FormattedMessage id="surveyButton" defaultMessage="Survey" />
+            </a>
+          </button>
+        </div>
       </div>
     </div>
   );
