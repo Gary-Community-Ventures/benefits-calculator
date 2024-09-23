c8b5fa6c35b588c474cc5f90fd1a74de17046b99
h0und <79583632+msrezaie@users.noreply.github.com>
2024-09-23 13:49:10 -0400

    added react gtm package

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,15 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import Wrapper from './Components/Wrapper/Wrapper';
 import './index.css';
+import TagManager from 'react-gtm-module';
+
+const gtmId = process.env.REACT_APP_GOOGLE_ANALYTICS_ID;
+
+if (gtmId) {
+  TagManager.initialize({ gtmId });
+} else {
+  console.error('REACT_APP_GOOGLE_ANALYTICS_ID is not defined. Google Tag Manager will not be initialized.');
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
