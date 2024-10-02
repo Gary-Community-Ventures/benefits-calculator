0d95801e71a70989c5aea5a71a2f7f3eecf1a5cb
Ivonne Hernandez <ivonne702@gmail.com>
2024-10-02 15:36:57 -0600

    Fix format

--- src/Components/ContinueButton/FormContinueButton.tsx
+++ src/Components/ContinueButton/FormContinueButton.tsx
@@ -1,16 +1,16 @@
-import { Button } from "@mui/material";
-import { FormattedMessageType } from "../../Types/Questions";
+import { Button } from '@mui/material';
+import { FormattedMessageType } from '../../Types/Questions';
 
 type FormContinueButtonProps = {
   formattedMessage: FormattedMessageType;
 };
 
-const FormContinueButton = ({formattedMessage}: FormContinueButtonProps) => {
+const FormContinueButton = ({ formattedMessage }: FormContinueButtonProps) => {
   return (
     <Button variant="contained" type="submit">
       {formattedMessage}
     </Button>
   );
-}
+};
 
 export default FormContinueButton;
