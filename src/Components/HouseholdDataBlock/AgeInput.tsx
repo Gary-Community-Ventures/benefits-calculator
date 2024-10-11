1fa060cab40e2b00f0ae5d6c662aa72875132f7f
Caleb <caleblpena@gmail.com>
2024-10-11 10:00:58 -0600

    translate months

--- src/Components/HouseholdDataBlock/AgeInput.tsx
+++ src/Components/HouseholdDataBlock/AgeInput.tsx
@@ -14,18 +14,18 @@ import AutoComplete from '../AutoComplete/AutoComplete';
 import './AgeInput.css';
 
 const MONTHS = {
-  1: 'January',
-  2: 'Febuary',
-  3: 'March',
-  4: 'April',
-  5: 'May',
-  6: 'June',
-  7: 'July',
-  8: 'August',
-  9: 'September',
-  10: 'October',
-  11: 'November',
-  12: 'December',
+  1: <FormattedMessage id="ageInput.months.january" defaultMessage='January' />,
+  2: <FormattedMessage id="ageInput.months.febuary" defaultMessage='Febuary' />,
+  3: <FormattedMessage id="ageInput.months.march" defaultMessage='March' />,
+  4: <FormattedMessage id="ageInput.months.april" defaultMessage='April' />,
+  5: <FormattedMessage id="ageInput.months.may" defaultMessage='May' />,
+  6: <FormattedMessage id="ageInput.months.june" defaultMessage='June' />,
+  7: <FormattedMessage id="ageInput.months.july" defaultMessage='July' />,
+  8: <FormattedMessage id="ageInput.months.august" defaultMessage='August' />,
+  9: <FormattedMessage id="ageInput.months.september" defaultMessage='September' />,
+  10: <FormattedMessage id="ageInput.months.october" defaultMessage='October' />,
+  11: <FormattedMessage id="ageInput.months.november" defaultMessage='November' />,
+  12: <FormattedMessage id="ageInput.months.december" defaultMessage='December' />,
 };
 
 const date = new Date();
