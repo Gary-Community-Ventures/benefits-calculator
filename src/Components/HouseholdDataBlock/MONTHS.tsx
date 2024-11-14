4f79f13a1c198bbdbca51aab99facf7c9a5f5b6d
Ivonne Hernandez <ivonne702@gmail.com>
2024-11-14 15:35:37 -0700

    Add a MONTHS file

---
+++ src/Components/HouseholdDataBlock/MONTHS.tsx
@@ -0,0 +1,16 @@
+import { FormattedMessage } from "react-intl";
+
+export const MONTHS = {
+  1: <FormattedMessage id="ageInput.months.january" defaultMessage="January" />,
+  2: <FormattedMessage id="ageInput.months.february" defaultMessage="February" />,
+  3: <FormattedMessage id="ageInput.months.march" defaultMessage="March" />,
+  4: <FormattedMessage id="ageInput.months.april" defaultMessage="April" />,
+  5: <FormattedMessage id="ageInput.months.may" defaultMessage="May" />,
+  6: <FormattedMessage id="ageInput.months.june" defaultMessage="June" />,
+  7: <FormattedMessage id="ageInput.months.july" defaultMessage="July" />,
+  8: <FormattedMessage id="ageInput.months.august" defaultMessage="August" />,
+  9: <FormattedMessage id="ageInput.months.september" defaultMessage="September" />,
+  10: <FormattedMessage id="ageInput.months.october" defaultMessage="October" />,
+  11: <FormattedMessage id="ageInput.months.november" defaultMessage="November" />,
+  12: <FormattedMessage id="ageInput.months.december" defaultMessage="December" />,
+};
