784ff64f1b91d62e981e57a8575fb71c82be786a
Caleb <caleblpena@gmail.com>
2024-12-06 10:24:53 -0700

    hide state select step

--- src/Components/Steps/SelectStatePage.tsx
+++ src/Components/Steps/SelectStatePage.tsx
@@ -14,7 +14,7 @@ import ErrorMessageWrapper from '../ErrorMessage/ErrorMessageWrapper';
 
 // This will get removed once NC is moved into the main server
 export const STATES: { [key: string]: string } =
-  process.env.REACT_APP_STATE === 'CO' ? { co: 'Colorado', nc: 'North Carolina' } : { nc: 'North Carolina' };
+  process.env.REACT_APP_STATE === 'CO' ? { co: 'Colorado' } : { nc: 'North Carolina' };
 
 const SelectStatePage = () => {
   const { formData, setFormData } = useContext(Context);
