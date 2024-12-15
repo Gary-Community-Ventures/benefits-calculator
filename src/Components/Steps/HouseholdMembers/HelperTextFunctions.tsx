8275adc8270ad5d59c199dec4b27f5400f264e23
Ivonne Hernandez <ivonne702@gmail.com>
2024-12-15 10:39:25 -0700

    Move helper text functions out of the hhmf

---
+++ src/Components/Steps/HouseholdMembers/HelperTextFunctions.tsx
@@ -0,0 +1,47 @@
+import { FormattedMessage } from "react-intl";
+import { MessageFunction } from "../../../Types/ErrorController";
+import ErrorMessageWrapper from "../../ErrorMessage/ErrorMessageWrapper";
+
+export const renderBirthMonthHelperText = () => {
+  return (
+    <ErrorMessageWrapper fontSize="1rem">
+      <FormattedMessage id="ageInput.month.error" defaultMessage="Please enter a birth month." />
+    </ErrorMessageWrapper>
+  );
+};
+
+export const renderBirthYearHelperText = () => {
+  return (
+    <ErrorMessageWrapper fontSize="1rem">
+      <FormattedMessage id="ageInput.year.error" defaultMessage="Please enter a birth year." />
+    </ErrorMessageWrapper>
+  );
+};
+
+export const renderHealthInsuranceHelperText = () => {
+  return (
+    <ErrorMessageWrapper fontSize="1.5rem">
+      <FormattedMessage
+        id="validation-helperText.healthInsurance"
+        defaultMessage='If none of these apply, please select "One or more household member(s) do not have health insurance"'
+      />
+    </ErrorMessageWrapper>
+  );
+};
+
+export const renderRelationshipToHHHelperText = () => {
+  return (
+    <ErrorMessageWrapper fontSize="1rem">
+      <FormattedMessage id="errorMessage-HHMemberRelationship" defaultMessage="Please select a relationship" />
+    </ErrorMessageWrapper>
+  );
+};
+
+export const renderIncomeFrequencyHelperText: MessageFunction<string> = () => {
+  return (
+    <ErrorMessageWrapper fontSize="1rem">
+      <FormattedMessage id="errorMessage-incomeFrequency" defaultMessage="Please select a frequency" />
+    </ErrorMessageWrapper>
+  );
+};
+
