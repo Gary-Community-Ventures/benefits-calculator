e0d895e78dc01c4925392931c7f838bb49ff6810
Caleb <caleblpena@gmail.com>
2025-01-06 15:19:03 -0700

    update numInputHelpers type

--- src/Assets/numInputHelpers.tsx
+++ src/Assets/numInputHelpers.tsx
@@ -1,18 +1,18 @@
 import { InputBaseComponentProps } from '@mui/material';
-import { ChangeEvent } from 'react';
+import { ChangeEvent, ReactEventHandler } from 'react';
 
 // export const NUM_PAD_PROPS: InputBaseComponentProps = { pattern: '[0-9]* .', inputMode: 'decimal' };
 export const NUM_PAD_PROPS: InputBaseComponentProps = { inputMode: 'decimal' };
 
-type InputChangeEvent = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+type EventElements = HTMLInputElement | HTMLTextAreaElement;
 
 export const INTEGERS = /^\d*$/; // any numbers
 export const DECIMALS = /^\d*\.?\d*$/; // any numbers with unlimited decimal places
 export const DOLLARS = /^\d*\.?\d{0,2}$/; // any numbers with a maximum of 2 decimal places
 
 // restrict what a user can enter into a text field
-export function handleNumbersOnly(onChange: (event: InputChangeEvent) => void, regex: RegExp = INTEGERS) {
-  return (event: InputChangeEvent) => {
+export function handleNumbersOnly(onChange: ReactEventHandler<EventElements> = () => {}, regex: RegExp = INTEGERS) {
+  return (event: ChangeEvent<EventElements>) => {
     const value = event.target.value;
 
     if (!regex.test(value)) {
