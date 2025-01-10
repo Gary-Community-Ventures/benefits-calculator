d60219770a0175a60c0de87bec1e5115625d581f
Ivonne Hernandez <ivonne702@gmail.com>
2025-01-10 04:53:22 -0700

    Add Larimer and Teller county to referrerDataInfo

--- src/Components/Referrer/referrerDataInfo.tsx
+++ src/Components/Referrer/referrerDataInfo.tsx
@@ -13,6 +13,8 @@ import DHSMFBLogo from '../../Assets/States/CO/WhiteLabels/DenverHumanServices/d
 import CCIGLogo from '../../Assets/States/CO/WhiteLabels/CCIG/ccigLogo.png';
 import ECMFBLogo from '../../Assets/States/CO/WhiteLabels/EagleCounty/ECMFBLogo.png';
 import ACHSMFBLogo from '../../Assets/States/CO/WhiteLabels/AdamsCountyHumanServices/achs.png';
+import LC_MFBLogo from '../../Assets/States/CO/WhiteLabels/LarimerCounty/LC_MFBLogo.png'
+import TC_MFBLogo from '../../Assets/States/CO/WhiteLabels/TellerCounty/TC_MFBLogo.png'
 
 const logoMap: { [key: string]: string | undefined } = {
   MFB_COLogo: MFBCOLogo,
@@ -30,6 +32,8 @@ const logoMap: { [key: string]: string | undefined } = {
   CCIG_Logo: CCIGLogo,
   EC_MFBLogo: ECMFBLogo,
   ACHS_MFBLogo: ACHSMFBLogo,
+  LC_MFBLogo: LC_MFBLogo,
+  TC_MFBLogo: TC_MFBLogo
 };
 
 export const renderLogoSource = (sourceLabel: string, logoAlt: string, logoClass: string) => {
