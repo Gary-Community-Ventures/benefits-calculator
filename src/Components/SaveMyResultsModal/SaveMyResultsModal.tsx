8689e294523e433e8616db70b8e565968ab1a063
Ivonne Hernandez <ivonne702@gmail.com>
2025-01-22 15:50:09 -0700

    Move the email and phone textfields out of SaveMyResultsModal

--- src/Components/SaveMyResultsModal/SaveMyResultsModal.tsx
+++ src/Components/SaveMyResultsModal/SaveMyResultsModal.tsx
@@ -1,17 +1,12 @@
-import { Snackbar, TextField } from '@mui/material';
+import { Snackbar } from '@mui/material';
 import CheckIcon from '@mui/icons-material/Check';
 import CloseIcon from '@mui/icons-material/Close';
 import LinkIcon from '@mui/icons-material/Link';
 import IconButton from '@mui/material/IconButton';
-import { forwardRef, useContext, useState } from 'react';
+import { forwardRef, useState } from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { Controller, useForm } from 'react-hook-form';
-import ErrorMessageWrapper from '../ErrorMessage/ErrorMessageWrapper';
-import { Context } from '../Wrapper/Wrapper';
-import { useParams } from 'react-router-dom';
-import { postMessage } from '../../apiCalls';
+import EmailTextfield from './EmailTextfield';
+import PhoneTextfield from './PhoneTextfield';
 import './SaveMyResultsModal.css';
 
 type SaveMyResultsModalProps = {
@@ -19,10 +14,7 @@ type SaveMyResultsModalProps = {
 };
 
 const SaveMyResultsModal = forwardRef(function SaveMyResultsModal({ close }: SaveMyResultsModalProps, ref) {
-  const { formData } = useContext(Context);
-  const { uuid } = useParams();
   const intl = useIntl();
-  const { formatMessage } = useIntl();
   const [copied, setCopied] = useState(false);
   const closeAriaLabelProps = {
     id: 'emailResults.close-AL',
@@ -33,71 +25,18 @@ const SaveMyResultsModal = forwardRef(function SaveMyResultsModal({ close }: Sav
     message: '',
   });
 
-  const formSchema = z.object({
-    email: z
-      .string({
-        errorMap: () => {
-          return {
-            message: formatMessage({
-              id: 'validation-helperText.email',
-              defaultMessage: 'Please enter a valid email address',
-            }),
-          };
-        },
-      })
-      .email()
-      .trim(),
-    // .or(z.literal('')),
-    // phone: z
-    //   .string({
-    //     errorMap: () => {
-    //       return {
-    //         message: formatMessage({
-    //           id: 'validation-helperText.phoneNumber',
-    //           defaultMessage: 'Please enter a 10 digit phone number',
-    //         }),
-    //       };
-    //     },
-    //   })
-    //   .trim()
-    //   .transform((value) => {
-    //     let newString = '';
-
-    //     for (const char of value) {
-    //       if (/\d/.test(char)) {
-    //         newString += char;
-    //       }
-    //     }
-
-    //     return newString;
-    //   })
-    //   .refine((value) => value.length === 0 || value.length === 10, {
-    //     message: formatMessage({
-    //       id: 'validation-helperText.phoneNumber',
-    //       defaultMessage: 'Please enter a 10 digit phone number',
-    //     }),
-    //   }),
-  });
-  // .refine((data) => data.email !== '' || data.phone !== '', {
-  //   // check this validation message
-  //   message: formatMessage({
-  //     id: 'validation-helperText.noEmailOrPhoneNumber',
-  //     defaultMessage: 'Please enter an email or phone number',
-  //   }),
-  // });
-
-  const {
-    control,
-    formState: { errors, isSubmitted },
-    trigger,
-    handleSubmit,
-  } = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: formData.signUpInfo.email ?? '',
-      // phone: formData.signUpInfo.phone ?? '',
-    },
-  });
+  const action = (
+    <>
+      <IconButton
+        size="small"
+        aria-label={intl.formatMessage(closeAriaLabelProps)}
+        color="inherit"
+        onClick={() => setSnackbar({ ...snackbar, open: false })}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </>
+  );
 
   const copyLink = () => {
     navigator.clipboard.writeText(window.location.href);
@@ -124,61 +63,6 @@ const SaveMyResultsModal = forwardRef(function SaveMyResultsModal({ close }: Sav
     );
   };
 
-  const submitHandler = async (data: z.infer<typeof formSchema>, event: Event) => {
-    console.log({ data }); //will have {email: X, phone: Y}
-    const buttonId = event.target?.id;
-    const { email, phone } = data;
-    let snackbarMessage = '';
-    let messageBody = {};
-    // TODO: maybe move the handler to inside of each button?
-
-    if (buttonId === 'email') {
-      snackbarMessage = intl.formatMessage({
-        id: 'emailResults.return-signupCompleted-email',
-        defaultMessage:
-          'A copy of your results have been sent. If you do not see the email in your inbox, please check your spam folder.',
-      });
-
-      messageBody = {
-        screen: uuid,
-        email: email,
-        type: 'emailScreen',
-      };
-    } else if (buttonId === 'phone') {
-      snackbarMessage = intl.formatMessage({
-        id: 'emailResults.return-signupCompleted',
-        defaultMessage: 'A copy of your results have been sent.',
-      });
-
-      messageBody = {
-        screen: uuid,
-        phone: phone,
-        type: 'textScreen',
-      };
-    }
-
-    try {
-      const message = messageBody;
-      await postMessage(message);
-      setSnackbar({ open: true, message: snackbarMessage });
-    } catch (error) {
-      throw new Error(`${error}`);
-    }
-  };
-
-  const action = (
-    <>
-      <IconButton
-        size="small"
-        aria-label={intl.formatMessage(closeAriaLabelProps)}
-        color="inherit"
-        onClick={() => setSnackbar({ ...snackbar, open: false })}
-      >
-        <CloseIcon fontSize="small" />
-      </IconButton>
-    </>
-  );
-
   return (
     <div className="email-results-container">
       <Snackbar
@@ -201,73 +85,8 @@ const SaveMyResultsModal = forwardRef(function SaveMyResultsModal({ close }: Sav
         <CloseIcon />
       </IconButton>
       {displayCopyResults()}
-      <form onSubmit={handleSubmit(submitHandler)}>
-        <div className="flex-direction-column">
-          <article className="bottom-margin">
-            <FormattedMessage id="emailResults.enter-emailLabel" defaultMessage="Email my results" />
-          </article>
-          <Controller
-            name="email"
-            control={control}
-            rules={{ required: false }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label={<FormattedMessage id="signUp.createEmailTextfield-label" defaultMessage="Email" />}
-                variant="outlined"
-                error={errors.email !== undefined}
-                helperText={
-                  errors.email !== undefined && (
-                    <ErrorMessageWrapper fontSize="1rem">{errors.email.message}</ErrorMessageWrapper>
-                  )
-                }
-                onChange={(...args) => {
-                  field.onChange(...args);
-                  if (isSubmitted) {
-                    // trigger(['email', 'phone']);
-                    trigger(['email']);
-                  }
-                }}
-              />
-            )}
-          />
-          <button className="send-button" type="submit" onClick={handleSubmit(submitHandler)}>
-            <FormattedMessage id="emailResults.sendButton" defaultMessage="Send" />
-          </button>
-        </div>
-        {/* <div className="flex-direction-column">
-          <article className="bottom-margin">
-            <FormattedMessage id="emailResults.enter-phoneNumberLabel" defaultMessage="Text my results" />
-          </article>
-          <Controller
-            name="phone"
-            control={control}
-            rules={{ required: false }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                label={<FormattedMessage id="signUp.createPhoneTextfield-label" defaultMessage="Cell Phone" />}
-                variant="outlined"
-                error={errors.phone !== undefined}
-                helperText={
-                  errors.phone !== undefined && (
-                    <ErrorMessageWrapper fontSize="1rem">{errors.phone.message}</ErrorMessageWrapper>
-                  )
-                }
-                onChange={(...args) => {
-                  field.onChange(...args);
-                  if (isSubmitted) {
-                    trigger(['email', 'phone']);
-                  }
-                }}
-              />
-            )}
-          />
-          <button className="send-button" type="submit" onClick={handleSubmit(submitHandler)}>
-            <FormattedMessage id="emailResults.sendButton" defaultMessage="Send" />
-          </button>
-        </div> */}
-      </form>
+      <EmailTextfield setSnackbar={setSnackbar} />
+      <PhoneTextfield setSnackbar={setSnackbar} />
     </div>
   );
 });
