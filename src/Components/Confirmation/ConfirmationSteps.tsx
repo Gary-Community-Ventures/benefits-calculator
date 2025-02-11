9a7f5b61b7c0e217e5a2408842f8b7aeb1516629
Ivonne Hernandez <ivonne702@gmail.com>
2025-02-11 15:47:29 -0700

    Add EnergyCalculatorApplianceStatus to ConfirmationSteps

--- src/Components/Confirmation/ConfirmationSteps.tsx
+++ src/Components/Confirmation/ConfirmationSteps.tsx
@@ -18,6 +18,7 @@ import { Benefits as BenefitsType } from '../../Types/FormData';
 import EnergyCalculatorElectricityProvider from '../EnergyCalculator/ConfirmationPage/ElectricityProvider';
 import EnergyCalculatorGasProvider from '../EnergyCalculator/ConfirmationPage/GasProvider';
 import EnergyCalculatorExpenses from '../EnergyCalculator/ConfirmationPage/Expenses';
+import EnergyCalculatorApplianceStatus from '../EnergyCalculator/ConfirmationPage/ApplianceStatus';
 
 function ZipCode() {
   const { formData } = useContext(Context);
@@ -360,6 +361,7 @@ const STEP_CONFIRMATIONS: Record<QuestionName, ReactNode | null> = {
   ),
   energyCalculatorGasProvider: <EnergyCalculatorGasProvider key="energyCalculatorGasProvider" />,
   energyCalculatorExpenses: <EnergyCalculatorExpenses key="energyCalculatorExpenses" />,
+  energyCalculatorApplianceStatus: <EnergyCalculatorApplianceStatus key="energyCalculatorApplianceStatus" />,
   signUpInfo: null,
 };
 
