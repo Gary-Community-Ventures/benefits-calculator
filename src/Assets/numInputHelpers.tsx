7294cf8b7fbfbfa12c6185107c1a2c0b51c90f72
Caleb <caleblpena@gmail.com>
2025-02-14 10:12:06 -0700

    Refactor handleNumbersOnly function signature

--- src/Assets/numInputHelpers.tsx
+++ src/Assets/numInputHelpers.tsx
@@ -9,12 +9,14 @@ export const INTEGERS = /^\d*$/; // any numbers
 export const DECIMALS = /^\d*\.?\d*$/; // any numbers with unlimited decimal places
 export const DOLLARS = /^\d*\.?\d{0,2}$/; // any numbers with a maximum of 2 decimal places
 
+type OnChangeFunction<EventElement extends EventElements> = (func: ChangeEvent<EventElement>) => void;
+
 // restrict what a user can enter into a text field
 export function handleNumbersOnly<EventElement extends EventElements>(
-  onChange: (func: ChangeEvent<EventElement>) => void = () => {},
+  onChange: OnChangeFunction<EventElement> = () => {},
   regex: RegExp = INTEGERS,
-) {
-  return (event: ChangeEvent<EventElement>) => {
+): OnChangeFunction<EventElement> {
+  return (event) => {
     const value = event.target.value;
 
     if (!regex.test(value)) {
