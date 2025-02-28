f1d13a139bd5e3a95ba0ff5c7520155a8cc91b4a
Sonali Bedge <sonali@codethedream.org>
2025-02-28 10:47:31 -0800

    Added <formattedMessage /> for translation purpose

--- src/Components/Results/NcLink211Message.tsx
+++ src/Components/Results/NcLink211Message.tsx
@@ -1,14 +1,17 @@
-import { useResultsContext } from './Results';
+import { FormattedMessage } from 'react-intl';
 
-export default function Link211Message1() {  
-  const { needs } = useResultsContext();   
-    
-  return (
-    <div>
-      <p>For more information or to learn more, please visit the following link  :  
-      <a href="https://nc211.org/" target="_blank" rel="noopener noreferrer">
-         Click here
-      </a></p>
+export default function Link211Message1() {   
+    return (
+    <div>      
+      <p>
+        <FormattedMessage
+          id="link211.message"
+          defaultMessage="For more information or to learn more, please visit the following link :"
+        />
+        <a href="https://nc211.org/" target="_blank" rel="noopener noreferrer">
+          <FormattedMessage id="link211.clickHere" defaultMessage="Click here" />
+        </a>
+      </p>      
     </div>
   );   
 }
