ebc06ea386c539c96431d22de9daec9107ca02c6
Caleb <caleblpena@gmail.com>
2025-03-04 15:55:08 -0700

    Add comment for whiteLabel routing logic

--- src/Components/RouterUtil/WhiteLabelRouter.tsx
+++ src/Components/RouterUtil/WhiteLabelRouter.tsx
@@ -2,6 +2,7 @@ import { Navigate, useParams } from 'react-router-dom';
 import { ALL_VALID_WHITE_LABELS, WhiteLabel } from '../../Types/WhiteLabel';
 import { useQueryString } from '../QuestionComponents/questionHooks';
 
+// route the path /:whiteLabel to /:whiteLabel/step-1
 export default function WhiteLabelRouter() {
   const { whiteLabel } = useParams();
   const query = useQueryString();
