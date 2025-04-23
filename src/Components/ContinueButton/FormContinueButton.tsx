f54c6d3626ff37b0fb1dd146eeca1faaba8c6195
Katie Brey <katie@codethedream.org>
2025-04-23 10:15:37 -0400

    fix button width, change to block comment

--- src/Components/ContinueButton/FormContinueButton.tsx
+++ src/Components/ContinueButton/FormContinueButton.tsx
@@ -1,4 +1,4 @@
-import { Button, CircularProgress } from '@mui/material';
+import { Box, Button, CircularProgress } from '@mui/material';
 import { PropsWithChildren, useContext } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Context } from '../Wrapper/Wrapper';
@@ -7,11 +7,14 @@ const FormContinueButton = ({ children }: PropsWithChildren) => {
   const { stepLoading } = useContext(Context);
   return (
     <Button variant="contained" type="submit">
-      {stepLoading ? (
-        <CircularProgress size="1rem" color="inherit" />
-      ) : (
-        children ?? <FormattedMessage id="continueButton" defaultMessage="Continue" />
-      )}
+      {stepLoading && <CircularProgress size="1rem" color="inherit" sx={{ position: 'absolute' }} />}
+      <span
+        style={{
+          visibility: stepLoading ? 'hidden' : 'visible',
+        }}
+      >
+        {children ?? <FormattedMessage id="continueButton" defaultMessage="Continue" />}
+      </span>
     </Button>
   );
 };
--- src/Components/Steps/stepForm.tsx
+++ src/Components/Steps/stepForm.tsx
@@ -4,13 +4,15 @@ import { Context } from '../Wrapper/Wrapper';
 import { QuestionName } from '../../Types/Questions';
 import { useGoToNextStep } from '../QuestionComponents/questionHooks';
 
-// This hook is used to create a form for screener steps.
-// By default, it will navigate to the next step when the form is submitted successfully.
-// The onSubmitSuccessfulOverride callback should be used when custom logic is needed.
-//
-// If you must navigate within the submit handler, use the setStepLoading function
-// to set the loading state to false before navigating. Otherwise, the isSubmitSuccessful
-// state will not be updated correctly and the loading state will not be set to false.
+/**
+ * This hook is used to create a form for screener steps.
+ * By default, it will navigate to the next step when the form is submitted successfully.
+ * The onSubmitSuccessfulOverride callback should be used when custom logic is needed.
+ *
+ * If you must navigate within the submit handler, use the setStepLoading function
+ * to set the loading state to false before navigating. Otherwise, the isSubmitSuccessful
+ * state will not be updated correctly and the loading state will not be set to false.
+ */
 export default function useStepForm<T extends FieldValues>({
   questionName,
   onSubmitSuccessfulOverride,
