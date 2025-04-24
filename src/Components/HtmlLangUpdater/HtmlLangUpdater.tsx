25c985441234dae56a1f49be64abefb9723e13ac
msrezaie <79583632+msrezaie@users.noreply.github.com>
2025-04-24 11:48:53 -0400

    refactored HtmlLangUpdater

--- src/Components/HtmlLangUpdater/HtmlLangUpdater.tsx
+++ src/Components/HtmlLangUpdater/HtmlLangUpdater.tsx
@@ -1,9 +1,11 @@
 import { useContext, useEffect } from 'react';
 import { Context } from '../Wrapper/Wrapper';
+import LANGUAGE_OPTIONS, { Language } from '../../Assets/languageOptions';
 
 /**
- * This component updates the lang attribute on the 'index.html'
- * based on the langauge coming from 'locale' context.
+ * This component updates the 'lang' attribute on the HTML
+ * element to prevent Google Translate from overriding our
+ * translations, but only for languages from LANGUAGE_OPTIONS.
  */
 export const HtmlLangUpdater = () => {
   const { locale } = useContext(Context);
@@ -11,6 +13,20 @@ export const HtmlLangUpdater = () => {
   useEffect(() => {
     const htmlLang = locale.slice(0, 2);
     document.documentElement.setAttribute('lang', htmlLang);
+
+    if (htmlLang !== 'en') {
+      const supportedLanguageCodes = Object.keys(LANGUAGE_OPTIONS).map(
+        (langKey) => (langKey as Language).slice(0, 2)
+      );
+
+      if (supportedLanguageCodes.includes(htmlLang)) {
+        document.documentElement.setAttribute('translate', 'no');
+      } else {
+        document.documentElement.removeAttribute('translate');
+      }
+    } else {
+      document.documentElement.removeAttribute('translate');
+    }
 
   }, [locale]);
 
