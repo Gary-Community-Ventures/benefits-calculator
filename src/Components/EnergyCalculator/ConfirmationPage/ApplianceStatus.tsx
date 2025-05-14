eacadf3815f73fb98a0b13456980e24d3e94057b
Carlos Cano <carlos.cano2013@gmail.com>
2025-05-14 08:19:46 -0400

    Reverted change

--- src/Components/EnergyCalculator/ConfirmationPage/ApplianceStatus.tsx
+++ src/Components/EnergyCalculator/ConfirmationPage/ApplianceStatus.tsx
@@ -29,12 +29,10 @@ const ApplianceStatus = () => {
 
         return (
           <p style={{ marginBottom: '.5rem' }} key={applianceStatusName}>
-            {('props' in applianceStatusProps.text) ? (
-  <FormattedMessage
-    id={applianceStatusProps.text.props.id}
-    defaultMessage={applianceStatusProps.text.props.default_message}
-  />
-) : null}
+            <FormattedMessage
+              id={applianceStatusProps.text.props.id}
+              defaultMessage={applianceStatusProps.text.props.defaultMessage}
+            />
           </p>
         );
       });
