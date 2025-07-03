48e8e7ac4a99c8a29fbba461296eb20d153dc10c
Josh Mejia <josh@myfriendben.org>
2025-07-03 11:05:46 -0600

    bug: adjust heating provider error msg

--- src/Components/EnergyCalculator/Steps/GasProvider.tsx
+++ src/Components/EnergyCalculator/Steps/GasProvider.tsx
@@ -50,7 +50,7 @@ export default function GasProvider() {
       .min(1, {
         message: formatMessage({
           id: 'energyCalculator.gasProvider.errorMessage',
-          defaultMessage: 'Please select an gas provider',
+          defaultMessage: 'Please select a heating provider',
         }),
       })
       .refine(
@@ -65,7 +65,7 @@ export default function GasProvider() {
         {
           message: formatMessage({
             id: 'energyCalculator.gasProvider.errorMessage',
-            defaultMessage: 'Please select a gas provider',
+            defaultMessage: 'Please select a heating provider',
           }),
         },
       ),
